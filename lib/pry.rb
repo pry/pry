bb7ac4b294733dd9ec7f5948b8070d431e1e6669
John Mair <jrmair@gmail.com>
2011-02-22 02:03:42 +1300

    version 0.6.0, fixed file require ordering issue

--- lib/pry.rb
+++ lib/pry.rb
@@ -5,7 +5,13 @@ direc = File.dirname(__FILE__)
 
 require "method_source"
 require "readline"
-
-Dir["#{direc}/pry/*.rb"].each do |file|
-  require file
-end
+require "#{direc}/pry/version"
+require "#{direc}/pry/hooks"
+require "#{direc}/pry/print"
+require "#{direc}/pry/command_base"
+require "#{direc}/pry/commands"
+require "#{direc}/pry/prompts"
+require "#{direc}/pry/completion"
+require "#{direc}/pry/core_extensions"
+require "#{direc}/pry/pry_class"
+require "#{direc}/pry/pry_instance"
--- lib/pry/version.rb
+++ lib/pry/version.rb
@@ -1,3 +1,3 @@
 class Pry
-  VERSION = "0.5.9"
+  VERSION = "0.6.0"
 end
