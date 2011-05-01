797b636f3564d17150a0ae79b10c8ae69d61b17c
John Mair <jrmair@gmail.com>
2011-05-01 22:25:37 +1200

    first start at reorganization and classification of commands

---
+++ lib/pry/default_commands/input.rb
@@ -0,0 +1,38 @@
+class Pry
+  module DefaultCommands
+
+    Input = Pry::CommandSet.new :input do
+
+      command "!", "Clear the input buffer. Useful if the parsing process goes wrong and you get stuck in the read loop." do
+        output.puts "Input buffer cleared!"
+        opts[:eval_string].clear
+      end
+
+      command "hist", "Show and replay Readline history. Type `hist --help` for more info." do |*args|
+        hist_array = Readline::HISTORY.to_a
+
+        if args.empty?
+          text = add_line_numbers(hist_array.join("\n"), 0)
+          stagger_output(text)
+          next
+        end
+
+        opts = Slop.parse(args) do |opt|
+          opt.banner "Usage: hist [--replay START..END]\nView and replay history\ne.g hist --replay 2..8"
+          opt.on :r, :replay, 'The line (or range of lines) to replay.', true, :as => Range
+          opt.on :h, :help, 'Show this message.', :tail => true do
+            output.puts opt.help
+          end
+        end
+
+        next if opts.h?
+
+        actions = Array(hist_array[opts[:replay]]).join("\n") + "\n"
+        Pry.active_instance.input = StringIO.new(actions)
+      end
+
+
+    end
+
+  end
+end
