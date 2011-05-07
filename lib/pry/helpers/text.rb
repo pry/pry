c0a42482d603119baddd12faa23b920c5e72db2a
Rob Gleeson <therobot.johnny5@gmail.com>
2011-05-07 15:20:03 +0100

    Pry::Helpers::Text defines singleton methods only.

--- lib/pry/helpers/text.rb
+++ lib/pry/helpers/text.rb
@@ -4,8 +4,6 @@ class Pry
     # The methods defined on {Text} are available to custom commands via {Pry::CommandContext#text}.  
     module Text
  
-      extend self
-
       COLORS = 
       {
         "black"   => 0,
@@ -19,61 +17,65 @@ class Pry
         "white"   => 7
       }
 
-      COLORS.each_pair do |color, value|
-        define_method color do |text|
-          Pry.color ? "\033[0;#{30+value}m#{text}\033[0m" : text.to_s
+      class << self
+        
+        COLORS.each_pair do |color, value|
+          define_method color do |text|
+            Pry.color ? "\033[0;#{30+value}m#{text}\033[0m" : text.to_s
+          end
+
+          define_method "bright_#{color}" do |text|
+            Pry.color ? "\033[1;#{30+value}m#{text}\033[0m" : text.to_s
+          end
         end
 
-        define_method "bright_#{color}" do |text|
-          Pry.color ? "\033[1;#{30+value}m#{text}\033[0m" : text.to_s
+        alias_method :grey, :bright_black
+        alias_method :gray, :bright_black
+        
+
+        # Remove any color codes from _text_.
+        #
+        # @param  [String, #to_s] text
+        # @return [String] _text_ stripped of any color codes.
+        def strip_color text
+          text.to_s.gsub /\e\[.*?(\d)+m/, ''
+        end
+
+        # Returns _text_ as bold text for use on a terminal.  
+        # _Pry.color_ must be true for this method to perform any transformations.
+        #
+        # @param [String, #to_s] text
+        # @return [String] _text_ 
+        def bold text
+          Pry.color ? "\e[1m#{text}\e[0m" : text.to_s
+        end
+
+        # Executes _block_ with _Pry.color_ set to false.
+        #
+        # @param [Proc]
+        # @return [void]
+        def no_color &block
+          boolean = Pry.color
+          Pry.color = false
+          yield
+        ensure
+          Pry.color = boolean
+        end
+
+        # Returns _text_ in a numbered list, beginning at _offset_.
+        # 
+        # @param  [String, #each_line] text
+        # @param  [Fixnum] offset
+        # @return [String]
+        def with_line_numbers text, offset
+          lines = text.each_line.to_a
+          lines.each_with_index.map do |line, index|
+            adjusted_index = index + offset
+            "#{self.blue adjusted_index}: #{line}"
+          end.join
         end
       end
 
-      alias_method :grey, :bright_black
-      alias_method :gray, :bright_black
-      
-
-      # Remove any color codes from _text_.
-      #
-      # @param  [String, #to_s] text
-      # @return [String] _text_ stripped of any color codes.
-      def strip_color text
-        text.to_s.gsub /\e\[.*?(\d)+m/, ''
-      end
-
-      # Returns _text_ as bold text for use on a terminal.  
-      # _Pry.color_ must be true for this method to perform any transformations.
-      #
-      # @param [String, #to_s] text
-      # @return [String] _text_ 
-      def bold text
-        Pry.color ? "\e[1m#{text}\e[0m" : text.to_s
-      end
-
-      # Executes _block_ with _Pry.color_ set to false.
-      #
-      # @param [Proc]
-      # @return [void]
-      def no_color &block
-        boolean = Pry.color
-        Pry.color = false
-        yield
-      ensure
-        Pry.color = boolean
-      end
-
-      # Returns _text_ in a numbered list, beginning at _offset_.
-      # 
-      # @param  [String, #each_line] text
-      # @param  [Fixnum] offset
-      # @return [String]
-      def with_line_numbers text, offset
-        lines = text.each_line.to_a
-        lines.each_with_index.map do |line, index|
-          adjusted_index = index + offset
-          "#{self.blue adjusted_index}: #{line}"
-        end.join
-      end
     end
 
   end
