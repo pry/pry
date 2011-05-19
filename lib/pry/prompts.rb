d99cd347a1e330aa58bd7405f404bd5c5a607d64
Lee Jarvis <lee@jarvis.co>
2011-05-19 18:32:45 +0100

    fixed up style/indentation of Pry prompts

--- lib/pry/prompts.rb
+++ lib/pry/prompts.rb
@@ -1,31 +1,29 @@
 class Pry
 
-
   # The default prompt; includes the target and nesting level
   DEFAULT_PROMPT = [
-                    proc do |target_self, nest_level|
+    proc { |target_self, nest_level|
+      if nest_level == 0
+      "pry(#{Pry.view_clip(target_self)})> "
+      else
+      "pry(#{Pry.view_clip(target_self)}):#{Pry.view_clip(nest_level)}> "
+      end
+    },
 
-                      if nest_level == 0
-                        "pry(#{Pry.view_clip(target_self)})> "
-                      else
-                        "pry(#{Pry.view_clip(target_self)}):#{Pry.view_clip(nest_level)}> "
-                      end
-                    end,
-
-                    proc do |target_self, nest_level|
-                      if nest_level == 0
-                        "pry(#{Pry.view_clip(target_self)})* "
-                      else
-                        "pry(#{Pry.view_clip(target_self)}):#{Pry.view_clip(nest_level)}* "
-                      end
-                    end
-                   ]
+    proc { |target_self, nest_level|
+      if nest_level == 0
+        "pry(#{Pry.view_clip(target_self)})* "
+      else
+        "pry(#{Pry.view_clip(target_self)}):#{Pry.view_clip(nest_level)}* "
+      end
+    }
+  ]
 
   # A simple prompt - doesn't display target or nesting level
   SIMPLE_PROMPT = [proc { ">> " }, proc { ">* " }]
 
   SHELL_PROMPT = [
-                 proc { |target_self, _| "pry #{Pry.view_clip(target_self)}:#{Dir.pwd} $ " },
-                 proc { |target_self, _| "pry #{Pry.view_clip(target_self)}:#{Dir.pwd} * " }
-                ]
+    proc { |target_self, _| "pry #{Pry.view_clip(target_self)}:#{Dir.pwd} $ " },
+    proc { |target_self, _| "pry #{Pry.view_clip(target_self)}:#{Dir.pwd} * " }
+  ]
 end
