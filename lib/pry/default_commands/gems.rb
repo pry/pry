9f262b0696f7cf90ac18b8f795fef88cad51f880
David Palm <dpalm@elctech.com>
2011-06-01 08:47:14 +0200

    rubygems 1.8.x compatibility fixes applied to dev branch

--- lib/pry/default_commands/gems.rb
+++ lib/pry/default_commands/gems.rb
@@ -19,14 +19,18 @@ class Pry
       end
 
       command "gem-cd", "Change working directory to specified gem's directory.", :argument_required => true do |gem|
-        spec = Gem.source_index.find_name(gem).sort { |a,b| Gem::Version.new(b.version) <=> Gem::Version.new(a.version) }.first
+        specs = Gem::Specification.respond_to?(:each) ? Gem::Specification.find_all_by_name(gem) : Gem.source_index.find_name(gem)
+        spec  = specs.sort { |a,b| Gem::Version.new(b.version) <=> Gem::Version.new(a.version) }.first
         spec ? Dir.chdir(spec.full_gem_path) : output.puts("Gem `#{gem}` not found.")
       end
 
-
       command "gem-list", "List/search installed gems. (Optional parameter: a regexp to limit the search)" do |pattern|
         pattern = Regexp.new pattern.to_s, Regexp::IGNORECASE
-        gems    = Gem.source_index.find_name(pattern).group_by(&:name)
+        gems    = if Gem::Specification.respond_to?(:each)
+                    Gem::Specification.select{|spec| spec.name =~ pattern }.group_by(&:name)
+                  else
+                    Gem.source_index.gems.values.group_by(&:name).select { |gemname, specs| gemname =~ pattern }
+                  end
 
         gems.each do |gem, specs|
           specs.sort! do |a,b| 
