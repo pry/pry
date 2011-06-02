597f1cc68c0f31dc9d7d6be5fbefcef65e95ee81
John Mair <jrmair@gmail.com>
2011-06-03 03:26:49 +1200

    reorganized command tests into test_default_commands/ folder with separate test files for each command set

---
+++ test/test_default_commands/test_context.rb
@@ -0,0 +1,64 @@
+require 'helper'
+
+describe "Pry::DefaultCommands::Context" do
+  describe "cd" do
+    after do
+      $obj = nil
+    end
+
+    it 'should cd into simple input' do
+      b = Pry.binding_for(Object.new)
+      b.eval("x = :mon_ouie")
+
+      redirect_pry_io(InputTester.new("cd x", "$obj = self", "exit-all"), StringIO.new) do
+        b.pry
+      end
+
+      $obj.should == :mon_ouie
+    end
+
+    it 'should break out of session with cd ..' do
+      b = Pry.binding_for(:outer)
+      b.eval("x = :inner")
+
+      redirect_pry_io(InputTester.new("cd x", "$inner = self;", "cd ..", "$outer = self", "exit-all"), StringIO.new) do
+        b.pry
+      end
+      $inner.should == :inner
+      $outer.should == :outer
+    end
+
+    it 'should break out to outer-most session with cd /' do
+      b = Pry.binding_for(:outer)
+      b.eval("x = :inner")
+
+      redirect_pry_io(InputTester.new("cd x", "$inner = self;", "cd 5", "$five = self", "cd /", "$outer = self", "exit-all"), StringIO.new) do
+        b.pry
+      end
+      $inner.should == :inner
+      $five.should == 5
+      $outer.should == :outer
+    end
+
+    it 'should start a session on TOPLEVEL_BINDING with cd ::' do
+      b = Pry.binding_for(:outer)
+
+      redirect_pry_io(InputTester.new("cd ::", "$obj = self", "exit-all"), StringIO.new) do
+        5.pry
+      end
+      $obj.should == TOPLEVEL_BINDING.eval('self')
+    end
+
+    it 'should cd into complex input (with spaces)' do
+      o = Object.new
+      def o.hello(x, y, z)
+        :mon_ouie
+      end
+
+      redirect_pry_io(InputTester.new("cd hello 1, 2, 3", "$obj = self", "exit-all"), StringIO.new) do
+        o.pry
+      end
+      $obj.should == :mon_ouie
+    end
+  end
+end
