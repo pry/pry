7c29bffd6f49be6af2f0fa7010834a669705bf4a
John Mair <jrmair@gmail.com>
2011-06-10 02:18:24 +1200

    play command almost complete

--- lib/pry/extended_commands/experimental.rb
+++ lib/pry/extended_commands/experimental.rb
@@ -14,35 +14,52 @@ class Pry
       end
 
       command "play", "Play a string as input" do |*args|
-        Slop.parse!(args) do |opt|
+        opts = Slop.parse!(args) do |opt|
           opt.banner "Usage: play-method [--replay START..END] [--clear] [--grep PATTERN] [--help]\n"
 
           opt.on :l, :lines, 'The line (or range of lines) to replay.', true, :as => Range
-          opt.on :m, :method, 'Play a method.', true do |meth_name|
-            if (meth = get_method_object(meth_name, target, {})).nil?
-              output.puts "Invalid method name: #{meth_name}."
-              next
-            end
-            code, code_type = code_and_code_type_for(meth)
-            next if !code
+          opt.on :m, :method, 'Play a method.', true
 
-            range = opt.l? ? opt[:l] : (0..-1)
-
-            Pry.active_instance.input = StringIO.new(code[range])
-          end
-
-          opt.on :f, "file", 'The line (or range of lines) to replay.', true do |file_name|
-            text = File.read File.expand_path(file_name)
-            range = opt.l? ? opt[:l] : (0..-1)
-
-            Pry.active_instance.input = StringIO.new(text[range])
-          end
+          opt.on :f, "file", 'The line (or range of lines) to replay.', true
 
           opt.on :h, :help, "This message." do
             output.puts opt
           end
         end
+
+        if opts.m?
+          meth_name = opts[:m]
+          if (meth = get_method_object(meth_name, target, {})).nil?
+            output.puts "Invalid method name: #{meth_name}."
+            next
+          end
+          code, code_type = code_and_code_type_for(meth)
+          next if !code
+
+          range = opts.l? ? opts[:l] : (0..-1)
+
+          Pry.active_instance.input = StringIO.new(code_join(code.each_line.to_a[range]))
+        end
+
+        if opts.f?
+          text_array = File.readlines File.expand_path(opts[:f])
+          range = opts.l? ? opts[:l] : (0..-1)
+
+          Pry.active_instance.input = StringIO.new(code_join(text_array[range]))
+        end
+
       end
+
+      helpers do
+
+        def code_join(arr)
+          arr.map do |v|
+            v.empty? ? "\n" : v
+          end.join
+        end
+
+      end
+
     end
   end
 end
