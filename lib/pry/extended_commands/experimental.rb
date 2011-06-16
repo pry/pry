4a0329a03281294bfdcfef209bda8c0869540003
John Mair <jrmair@gmail.com>
2011-06-17 01:47:18 +1200

    fixed spacing on experimental.rb

--- lib/pry/extended_commands/experimental.rb
+++ lib/pry/extended_commands/experimental.rb
@@ -3,7 +3,6 @@ class Pry
 
     Experimental = Pry::CommandSet.new do
 
-
       command "reload-method", "Reload the source specifically for a method", :requires_gem => "method_reload" do |meth_name|
         if (meth = get_method_object(meth_name, target, {})).nil?
           output.puts "Invalid method name: #{meth_name}."
