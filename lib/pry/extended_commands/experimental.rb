552e50320cbf83f81cae6377259ed4766d0e4a84
John Mair <jrmair@gmail.com>
2011-06-18 06:02:38 +1200

    added blame command to Experimental

--- lib/pry/extended_commands/experimental.rb
+++ lib/pry/extended_commands/experimental.rb
@@ -12,6 +12,26 @@ class Pry
         meth.reload
       end
 
+      command "blame", "Show blame for a method", :requires_gem => "grit" do |meth_name|
+        if (meth = get_method_object(meth_name, target, {})).nil?
+          output.puts "Invalid method name: #{meth_name}."
+          next
+        end
+
+        repo ||= Grit::Repo.new(".")
+        start_line = meth.source_location.last
+        num_lines = meth.source.lines.count
+        authors = repo.blame(meth.source_location.first).lines.select do |v|
+          v.lineno >= start_line && v.lineno <= start_line + num_lines
+        end.map do |v|
+          v.commit.author.output(Time.new).split(/</).first.strip
+        end
+
+        lines_with_blame = []
+        meth.source.lines.zip(authors) { |line, author| lines_with_blame << ("#{author}".ljust(10) + colorize_code(line)) }
+        output.puts        lines_with_blame.join
+      end
+
     end
   end
 end
