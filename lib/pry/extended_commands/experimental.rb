3b04f4b582962ee7e6c379220c7911546284c58d
John Mair <jrmair@gmail.com>
2011-06-20 20:28:59 +1200

    added diff command to Experimental command set (temporary home, will remove soon to pry-git plugin)

--- lib/pry/extended_commands/experimental.rb
+++ lib/pry/extended_commands/experimental.rb
@@ -33,6 +33,45 @@ class Pry
         output.puts        lines_with_blame.join
       end
 
+      command "diff", "Show the diff for a method", :requires_gem => ["grit", "diffy"] do |meth_name|
+        require 'grit'
+        require 'diffy'
+
+        if (meth = get_method_object(meth_name, target, {})).nil?
+          output.puts "Invalid method name: #{meth_name}."
+          next
+        end
+
+        output.puts colorize_code(Diffy::Diff.new(method_code_from_head(meth), meth.source))
+      end
+
+      helpers do
+        def get_file_from_commit(path)
+          repo = Grit::Repo.new('.')
+          head = repo.commits.first
+          tree_names = path.split("/")
+          start_tree = head.tree
+          blob_name = tree_names.last
+          tree = tree_names[0..-2].inject(start_tree)  { |a, v|  a.trees.find { |t| t.basename == v } }
+          blob = tree.blobs.find { |v| v.basename == blob_name }
+          blob.data
+        end
+
+        def method_code_from_head(meth)
+          rel_path = relative_path(meth.source_location.first)
+          code = get_file_from_commit(rel_path)
+          start_line = meth.source_location.last
+          code_length = meth.source.lines.count
+          code.lines.to_a[(start_line - 1)...((start_line - 1) + code_length)].join
+        end
+
+        def relative_path(path)
+          path =~ /#{Regexp.escape(File.expand_path("."))}\/(.*)/
+          $1
+        end
+
+      end
+
     end
   end
 end
