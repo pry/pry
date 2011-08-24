f6758bfb4e7c6ab7f701129f3116fe8db56e6a8a
Conrad Irwin <conrad.irwin@gmail.com>
2011-08-24 01:07:52 -0700

    Do something sensible about uninspectable things

--- test/test_pry_output.rb
+++ test/test_pry_output.rb
@@ -27,4 +27,22 @@ describe Pry do
       mock_pry("1").should =~ /output error: failed to show result/
     end
   end
+
+  describe "DEFAULT_PRINT" do
+    it "should output the right thing" do
+      mock_pry("{:a => 1}").should =~ /\{:a=>1\}/
+    end
+
+    it "should not be phased by un-inspectable things" do
+      mock_pry("class NastyClass; undef pretty_inspect; end", "NastyClass.new").should =~ /#<NastyClass:0x[0-9a-f]+>/
+    end
+
+    it "should warn you about un-inspectable things" do
+      mock_pry("class NastyClass; undef pretty_inspect; end", "NastyClass.new").should =~ /output error: #<(NoMethodError|NameError): undefined method `pretty_inspect'/
+    end
+
+    it "should warn you when you have badly behaved objects" do
+      mock_pry("class UnCouth; def pretty_inspect(*a); :cussing_symbol; end; end", "UnCouth.new").should =~ /output error: .pretty_inspect didn't return a String/
+    end
+  end
 end
