e11a5cf47ef752e91581ef117bb7f2424fcb83bc
John Mair <jrmair@gmail.com>
2011-08-25 21:41:43 +1200

    reverted behaviour of exit and friends back to 1.9.3 behaviour (to prevent surprising users) also updated tests to reflect this

--- test/test_default_commands/test_context.rb
+++ test/test_default_commands/test_context.rb
@@ -1,29 +1,29 @@
 require 'helper'
 
 describe "Pry::DefaultCommands::Context" do
-  describe "quit" do
+  describe "exit-all" do
     it 'should break out of the repl loop of Pry instance (returning target of session)' do
-      redirect_pry_io(InputTester.new("quit"), StringIO.new) do
+      redirect_pry_io(InputTester.new("exit-all"), StringIO.new) do
         Pry.new.repl(0).should == 0
       end
     end
 
     it 'should break out of the repl loop of Pry instance wth a user specified value' do
-      redirect_pry_io(InputTester.new("quit 'message'"), StringIO.new) do
+      redirect_pry_io(InputTester.new("exit-all 'message'"), StringIO.new) do
         Pry.new.repl(0).should == 'message'
       end
     end
 
-    it 'should quit break of the repl loop even if multiple bindings still on stack' do
+    it 'should break of the repl loop even if multiple bindings still on stack' do
       ins = nil
-      redirect_pry_io(InputTester.new("cd 1", "cd 2", "quit 'message'"), StringIO.new) do
+      redirect_pry_io(InputTester.new("cd 1", "cd 2", "exit-all 'message'"), StringIO.new) do
         ins = Pry.new.tap { |v| v.repl(0).should == 'message' }
       end
     end
 
     it 'binding_stack should be empty after breaking out of the repl loop' do
       ins = nil
-      redirect_pry_io(InputTester.new("cd 1", "cd 2", "quit"), StringIO.new) do
+      redirect_pry_io(InputTester.new("cd 1", "cd 2", "exit-all"), StringIO.new) do
         ins = Pry.new.tap { |v| v.repl(0) }
       end
 
@@ -31,10 +31,31 @@ describe "Pry::DefaultCommands::Context" do
     end
   end
 
+  describe "exit" do
+    it 'should pop a binding with exit' do
+      b = Pry.binding_for(:outer)
+      b.eval("x = :inner")
+
+      redirect_pry_io(InputTester.new("cd x", "$inner = self;", "exit", "$outer = self", "exit-all"), StringIO.new) do
+        b.pry
+      end
+      $inner.should == :inner
+      $outer.should == :outer
+    end
+
+    it 'should break out of the repl loop of Pry instance when binding_stack has only one binding with exit' do
+      Pry.start(0, :input => StringIO.new("exit")).should == 0
+    end
+
+    it 'should break out of the repl loop of Pry instance when binding_stack has only one binding with exit, and return user-given value' do
+      Pry.start(0, :input => StringIO.new("exit :john")).should == :john
+    end
+  end
+
   describe "jump-to" do
     it 'should jump to the proper binding index in the stack' do
       outp = StringIO.new
-      redirect_pry_io(InputTester.new("cd 1", "cd 2", "jump-to 1", "$blah = self", "quit"), outp) do
+      redirect_pry_io(InputTester.new("cd 1", "cd 2", "jump-to 1", "$blah = self", "exit-all"), outp) do
         Pry.start(0)
       end
 
@@ -43,7 +64,7 @@ describe "Pry::DefaultCommands::Context" do
 
     it 'should print error when trying to jump to a non-existent binding index' do
       outp = StringIO.new
-      redirect_pry_io(InputTester.new("cd 1", "cd 2", "jump-to 100", "quit"), outp) do
+      redirect_pry_io(InputTester.new("cd 1", "cd 2", "jump-to 100", "exit-all"), outp) do
         Pry.start(0)
       end
 
@@ -52,7 +73,7 @@ describe "Pry::DefaultCommands::Context" do
 
     it 'should print error when trying to jump to the same binding index' do
       outp = StringIO.new
-      redirect_pry_io(InputTester.new("cd 1", "cd 2", "jump-to 2", "quit"), outp) do
+      redirect_pry_io(InputTester.new("cd 1", "cd 2", "jump-to 2", "exit-all"), outp) do
         Pry.new.repl(0)
       end
 
@@ -60,9 +81,9 @@ describe "Pry::DefaultCommands::Context" do
     end
   end
 
-  describe "exit" do
+  describe "exit-program" do
     it 'should raise SystemExit' do
-      redirect_pry_io(InputTester.new("exit"), StringIO.new) do
+      redirect_pry_io(InputTester.new("exit-program"), StringIO.new) do
         lambda { Pry.new.repl(0).should == 0 }.should.raise SystemExit
       end
     end
@@ -95,7 +116,7 @@ describe "Pry::DefaultCommands::Context" do
       $outer.should == :outer
     end
 
-    it 'should break out of the repl loop of Pry instance when binding_stack has only one binding' do
+    it 'should break out of the repl loop of Pry instance when binding_stack has only one binding with cd ..' do
       # redirect_pry_io(InputTester.new("ls"), StringIO.new) do
       #   o =  Pry.new.tap { |v| v.repl(0) }
       # end
