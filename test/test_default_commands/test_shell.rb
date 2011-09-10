cc37c757882f8691723bf2b77db3729d827e6391
John Mair <jrmair@gmail.com>
2011-09-11 04:41:05 +1200

    cat --ex now shows code for exceptions raised in the REPL

--- test/test_default_commands/test_shell.rb
+++ test/test_default_commands/test_shell.rb
@@ -6,8 +6,8 @@ describe "Pry::DefaultCommands::Shell" do
     # this doesnt work so well on rbx due to differences in backtrace
     # so we currently skip rbx until we figure out a workaround
     if !rbx?
-      it 'cat --ex should give warning when exception is raised in repl' do
-        mock_pry("this raises error", "cat --ex").should =~ /Cannot cat exceptions raised in REPL/
+      it 'cat --ex should correctly display code that generated exception even if raised in repl' do
+        mock_pry("this raises error", "cat --ex").should =~ /\d+:(\s*) this raises error/
       end
 
       it 'cat --ex should correctly display code that generated exception' do
