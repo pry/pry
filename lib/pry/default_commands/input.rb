0200f1b6f473635f567b61e63af0e3f71d336241
John Mair <jrmair@gmail.com>
2011-09-12 04:26:38 +1200

    play -m now uses eval_string.replace() instead of changing Pry#input
    This is simpler and more transparent. Also added to this commit is `show-input` now invoked immediately after a `play -m meth --open` (this was only possible because of the eval_string change).
    Tests for `play` method also updated to reflect changes.

--- lib/pry/default_commands/input.rb
+++ lib/pry/default_commands/input.rb
@@ -79,7 +79,8 @@ e.g amend-line puts 'hello again'   # no line number modifies immediately preced
           range = opts.l? ? one_index_range_or_number(opts[:l]) : (0..-1)
           range = (0..-2) if opts.o?
 
-          _pry_.input = StringIO.new(Array(code.each_line.to_a[range]).join)
+          eval_string.replace((code.each_line.to_a[range]).join)
+          run "show-input" if opts.o?
         elsif opts.f?
           file_name = File.expand_path(opts[:f])
           next output.puts "No such file: #{opts[:f]}" if !File.exists?(file_name)
