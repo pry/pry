3549cc108bc3f212fd9238e2358261315f0f0e2f
Conrad Irwin <conrad.irwin@gmail.com>
2011-09-14 15:48:20 -0700

    Prefix fallback "output error" with "(pry)"
    robgleeson reported that the message could be confusing if you weren't
    expecting it.

--- test/test_pry_output.rb
+++ test/test_pry_output.rb
@@ -16,7 +16,7 @@ describe Pry do
     it "should display serialization exceptions" do
       Pry.config.print = lambda { |*a| raise "catch-22" }
 
-      mock_pry("1").should =~ /output error: #<RuntimeError: catch-22>/
+      mock_pry("1").should =~ /\(pry\) output error: #<RuntimeError: catch-22>/
     end
 
     it "should catch errors serializing exceptions" do
@@ -24,7 +24,7 @@ describe Pry do
         raise Exception.new("catch-22").tap{ |e| class << e; def inspect; raise e; end; end }
       end
 
-      mock_pry("1").should =~ /output error: failed to show result/
+      mock_pry("1").should =~ /\(pry\) output error: failed to show result/
     end
   end
 
