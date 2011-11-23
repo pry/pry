7bf54d04386f516a02581822849dde2aaacdd4d2
John Mair <jrmair@gmail.com>
2011-11-23 18:51:54 +1300

    Hook system now executes hooks in order.
    Also the return value of exec_hook() is now the value of the last executed hook.
    Internally, hooks now use arrays (rather than hash). Also added a get_hooks() method.
    Updated tests to reflect these changes.

--- lib/pry/hooks.rb
+++ lib/pry/hooks.rb
@@ -6,17 +6,20 @@ class Pry
     end
 
     # Add a new hook to be executed for the `name` even.
-    # @param [Symbol] name The name of the event.
-    # @param [Symbol] hook_function_name The name of the hook.
+    # @param [Symbol] event_name The name of the event.
+    # @param [Symbol] hook_name The name of the hook.
     # @param [#call] callable The callable.
     # @yield The block to use as the callable (if `callable` parameter not provided)
-    def add_hook(name, hook_function_name, callable=nil, &block)
-      @hooks[name] ||= {}
+    def add_hook(event_name, hook_name, callable=nil, &block)
+      @hooks[event_name] ||= []
+
+      # do not allow duplicates
+      raise ArgumentError, "Hook with name '#{hook_name}' already defined!" if hook_exists?(event_name, hook_name)
 
       if block
-        @hooks[name][hook_function_name] = block
+        @hooks[event_name] << [hook_name, block]
       elsif callable
-        @hooks[name][hook_function_name] = callable
+        @hooks[event_name] << [hook_name, callable]
       else
         raise ArgumentError, "Must provide a block or callable."
       end
@@ -24,47 +27,82 @@ class Pry
       self
     end
 
-    # Execute the list of hooks for the `name` event.
-    # @param [Symbol] name The name of the event.
+    # Execute the list of hooks for the `event_name` event.
+    # @param [Symbol] event_name The name of the event.
     # @param [Array] args The arguments to pass to each hook function.
-    # @return [Hash] The return values for each of the hook functions.
-    def exec_hook(name, *args, &block)
-      @hooks[name] ||= {}
-      Hash[@hooks[name].each.map { |k, v| [k, v.call(*args, &block)] }]
+    # @return [Object] The return value of the last executed hook.
+    def exec_hook(event_name, *args, &block)
+      @hooks[event_name] ||= []
+
+      # silence warnings to get rid of 1.8's "warning: multiple values
+      # for a block parameter" warnings
+      Pry::Helpers::BaseHelpers.silence_warnings do
+        @hooks[event_name].map { |hook_name, callable| callable.call(*args, &block) }.last
+      end
     end
 
-    # Return the number of hook functions registered for the `name` event.
-    # @param [Symbol] name The name of the event.
-    # @return [Fixnum] The number of hook functions for the `name` event.
-    def hook_count(name)
-      @hooks[name] ||= {}
-      @hooks[name].size
+    # Return the number of hook functions registered for the `event_name` event.
+    # @param [Symbol] event_name The name of the event.
+    # @return [Fixnum] The number of hook functions for `event_name`.
+    def hook_count(event_name)
+      @hooks[event_name] ||= []
+      @hooks[event_name].size
+    end
+
+    # Return a specific hook for a given event.
+    # @param [Symbol] event_name The name of the event.
+    # @param [Symbol[ hook_name The name of the hook
+    # @return [#call] The requested hook.
+    def get_hook(event_name, hook_name)
+      @hooks[event_name] ||= []
+      hook = @hooks[event_name].find { |current_hook_name, callable| current_hook_name == hook_name }
+      hook.last if hook
     end
 
     # Return the hash of hook names / hook functions for a
-    # given event.
-    # @param [Symbol] name The name of the event.
-    # @return [Hash]
-    def get_hook(name, hook_function_name)
-      @hooks[name] ||= {}
-      @hooks[name][hook_function_name]
+    # given event. (Note that modifying the returned hash does not
+    # alter the hooks, use add_hook/delete_hook for that).
+    # @param [Symbol] event_name The name of the event.
+    # @return [Hash] The hash of hook names / hook functions.
+    def get_hooks(event_name)
+      @hooks[event_name] ||= []
+      Hash[@hooks[event_name]]
     end
 
     # Delete a hook for an event.
-    # @param [Symbol] name The name of the event.
-    # @param [Symbol] hook_function_name The name of the hook.
+    # @param [Symbol] event_name The name of the event.
+    # @param [Symbol] hook_name The name of the hook.
     #   to delete.
     # @return [#call] The deleted hook.
-    def delete_hook(name, hook_function_name)
-      @hooks[name] ||= {}
-      @hooks[name].delete(hook_function_name)
+    def delete_hook(event_name, hook_name)
+      @hooks[event_name] ||= []
+      deleted_callable = nil
+
+      @hooks[event_name].delete_if do |current_hook_name, callable|
+        if current_hook_name == hook_name
+          deleted_callable = callable
+          true
+        else
+          false
+        end
+      end
+      deleted_callable
     end
 
     # Clear all hooks functions for a given event.
-    # @param [String] name The name of the event.
-    def clear(name)
-      @hooks[name] = {}
+    # @param [String] event_name The name of the event.
+    def clear(event_name)
+      @hooks[event_name] = []
     end
+
+    # @param [Symbol] event_name Name of the event.
+    # @param [Symbol] hook_name Name of the hook.
+    # @return [Boolean] Whether the hook by the name `hook_name`
+    #   is defined for the event.
+    def hook_exists?(event_name, hook_name)
+      !!@hooks[event_name].find { |name, _| name == hook_name }
+    end
+    private :hook_exists?
 
   end
 end
