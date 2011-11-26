b20ce2f08117cacf554df317bc23e700f33a6cac
Ryan Fitzgerald <rwfitzge@gmail.com>
2011-11-25 23:39:58 -0800

    cleanup and rbx fixes

--- lib/pry/pry_instance.rb
+++ lib/pry/pry_instance.rb
@@ -534,9 +534,10 @@ class Pry
     prompt_stack.size > 1 ? prompt_stack.pop : prompt
   end
 
-  # Determine if a string of code is a valid Ruby expression.
+  # Determine if a string of code is a complete Ruby expression.
   # @param [String] code The code to validate.
-  # @return [Boolean] Whether or not the code is a valid Ruby expression.
+  # @return [Boolean] Whether or not the code is a complete Ruby expression.
+  # @raise [SyntaxError] Any SyntaxError that does not represent incompleteness.
   # @example
   #   valid_expression?("class Hello") #=> false
   #   valid_expression?("class Hello; end") #=> true
@@ -547,11 +548,12 @@ class Pry
       Rubinius::Melbourne.parse_string(str, Pry.eval_path)
     else
       catch(:valid) {
-        eval("BEGIN{throw :valid, true}\n#{str}", binding, Pry.eval_path)
+        eval("BEGIN{throw :valid}\n#{str}", binding, Pry.eval_path)
       }
     end
+    true
   rescue SyntaxError => e
-    if incomplete_user_input_exception?(e, caller)
+    if incomplete_user_input_exception?(e)
       false
     else
       raise e
@@ -560,27 +562,16 @@ class Pry
 
   # Check whether the exception indicates that the user should input more.
   #
-  # The first part of the check verifies that the exception was raised from
-  # the input to the eval, taking care not to be confused if the input contains
-  # an eval() with incomplete syntax.
-  #
   # @param [SyntaxError] the exception object that was raised.
   # @param [Array<String>] The stack frame of the function that executed eval.
   # @return [Boolean]
   #
-  def incomplete_user_input_exception?(ex, stack)
-    # deliberate use of ^ instead of \A, error messages can be two lines long.
-    from_pry_input = /^#{Regexp.escape(Pry.eval_path)}/
-
-    return false unless SyntaxError === ex && ex.message =~ from_pry_input
-
+  def incomplete_user_input_exception?(ex)
     case ex.message
     when /unexpected (\$end|end-of-file|END_OF_FILE)/, # mri, jruby, ironruby
         /unterminated (quoted string|string|regexp) meets end of file/, # "quoted string" is ironruby
         /missing 'end' for/, /: expecting '[})\]]'$/, /can't find string ".*" anywhere before EOF/, /expecting keyword_end/ # rbx
-      backtrace = ex.backtrace
-      backtrace = backtrace.drop(1) if RUBY_VERSION =~ /^1\.8/ && RUBY_PLATFORM !~ /java/
-      backtrace.grep(from_pry_input).count <= stack.grep(from_pry_input).count
+      true
     else
       false
     end
