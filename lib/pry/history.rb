f0d1738797e2a1ef2ff5c31339bf046ed4a5d136
Ryan Fitzgerald <rwfitzge@gmail.com>
2011-12-01 21:03:36 -0800

    modularize History reading, writing, and readline integration

--- lib/pry/history.rb
+++ lib/pry/history.rb
@@ -1,31 +1,34 @@
 class Pry
   # The History class is responsible for maintaining the user's input history, both
-  # internally and within Readline::HISTORY.
+  # internally and within Readline.
   class History
+    attr_accessor :loader, :saver, :pusher, :clearer
+
     def initialize
       @history = []
       @saved_lines = 0
+
+      @loader  = method(:read_from_file)
+      @saver   = method(:write_to_file)
+      @pusher  = method(:push_to_readline)
+      @clearer = method(:clear_readline)
     end
 
-    # Loads a file's contents into the input history.
-    # @param [String] filename
+    # Load the input history using `History.loader`.
     # @return [Integer] The number of lines loaded
-    def load(filename)
-      File.foreach(filename) do |line|
-        Readline::HISTORY << line.chomp
+    def load
+      @loader.call do |line|
+        @pusher.call(line.chomp)
         @history << line.chomp
       end
       @saved_lines = @history.length
     end
 
-    # Appends input history from this session to a file.
-    # @param [String] filename
+    # Writes this session's history using `History.saver`.
     # @return [Integer] The number of lines saved
-    def save(filename)
+    def save
       history_to_save = @history[@saved_lines..-1]
-      File.open(filename, 'a') do |f|
-        history_to_save.each { |ln| f.puts ln }
-      end
+      @saver.call(history_to_save)
       @saved_lines = @history.length
       history_to_save.length
     end
@@ -35,7 +38,7 @@ class Pry
     # @return [String] The same line that was passed in
     def push(line)
       unless line.empty? || (@history.last && line == @history.last)
-        Readline::HISTORY << line
+        @pusher.call(line)
         @history << line
       end
       line
@@ -46,7 +49,7 @@ class Pry
     # saved, but anything they put in afterwards will still be appended to the
     # history file on exit.
     def clear
-      Readline::HISTORY.shift until Readline::HISTORY.empty?
+      @clearer.call
       @history = []
       @saved_lines = 0
     end
@@ -56,6 +59,37 @@ class Pry
     #   or entered by the user in the current session.
     def to_a
       @history.dup
+    end
+
+    private
+    # The default loader. Yields lines from `Pry.history.config.file`.
+    def read_from_file
+      history_file = File.expand_path(Pry.config.history.file)
+
+      if File.exists?(history_file)
+        File.foreach(history_file) { |line| yield(line) }
+      end
+    end
+
+    # The default saver. Appends the given lines to `Pry.history.config.file`.
+    # @param [Array<String>] lines
+    def write_to_file(lines)
+      history_file = File.expand_path(Pry.config.history.file)
+
+      File.open(history_file, 'a') do |f|
+        lines.each { |ln| f.puts ln }
+      end
+    end
+
+    # The default pusher. Appends the given line to Readline::HISTORY.
+    # @param [String] line
+    def push_to_readline(line)
+      Readline::HISTORY << line
+    end
+
+    # The default clearer. Clears Readline::HISTORY.
+    def clear_readline
+      Readline::HISTORY.shift until Readline::HISTORY.empty?
     end
   end
 end
