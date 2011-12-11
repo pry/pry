702a76727e3cc2844dbbd9dfec8f94f7ded4e367
John Mair <jrmair@gmail.com>
2011-12-11 21:16:09 +1300

    Added Pry.config.gist.inspecter config option.
    Useful for determining how expression output will be gisted.
    Also fixed some typos in config.rb for Pry.config.history docs

--- lib/pry/default_commands/documentation.rb
+++ lib/pry/default_commands/documentation.rb
@@ -87,7 +87,7 @@ class Pry
             corrected_index = index + normalized_range.first
             if code && code != ""
               content << code
-              content << "#{comment_expression_result_for_gist(_pry_.output_array[corrected_index].pretty_inspect)}" if code !~ /;\Z/
+              content << "#{comment_expression_result_for_gist(Pry.config.gist.inspecter.call(_pry_.output_array[corrected_index]))}" if code !~ /;\Z/
             end
           end
         elsif opts.present?(:doc)
