7c8aa38015677ed82d6031eb8ee61782d4b6efb1
John Mair <jrmair@gmail.com>
2012-01-24 01:06:23 +1300

    removed some unnecessary global vars from tests

--- test/test_default_commands/test_documentation.rb
+++ test/test_default_commands/test_documentation.rb
@@ -3,8 +3,7 @@ require 'helper'
 describe "Pry::DefaultCommands::Documentation" do
   describe "show-doc" do
     it 'should output a method\'s documentation' do
-      str_output = StringIO.new
-      redirect_pry_io(InputTester.new("show-doc sample_method", "exit-all"), str_output) do
+      redirect_pry_io(InputTester.new("show-doc sample_method", "exit-all"), str_output=StringIO.new) do
         pry
       end
 
@@ -12,20 +11,17 @@ describe "Pry::DefaultCommands::Documentation" do
     end
 
     it 'should output a method\'s documentation if inside method without needing to use method name' do
-      $str_output = StringIO.new
-
       o = Object.new
 
       # sample comment
       def o.sample
-        redirect_pry_io(InputTester.new("show-doc", "exit-all"), $str_output) do
+        redirect_pry_io(InputTester.new("show-doc", "exit-all"), $out=StringIO.new) do
           binding.pry
        end
       end
       o.sample
-
-      $str_output.string.should =~ /sample comment/
-      $str_output = nil
+      $out.string.should =~ /sample comment/
+      $out = nil
     end
 
     it "should be able to find super methods" do
