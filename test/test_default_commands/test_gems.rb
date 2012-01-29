fe4850eff123531e61cebbcf1a3c056e68d11bd6
Conrad Irwin <conrad.irwin@gmail.com>
2012-01-28 19:48:52 -0800

    Don't explode in gem-list [Fixes #453, #454]

--- test/test_default_commands/test_gems.rb
+++ test/test_default_commands/test_gems.rb
@@ -9,6 +9,10 @@ describe "Pry::DefaultCommands::Gems" do
       Pry.start self, :input => InputTester.new("gem-list", "exit-all"), :output => str_output
       str_output.string.should.not =~ /NoMethodError/
     end
+
+    it 'should not raise when invoked with an argument' do
+      mock_pry('gem-list pry').should.not =~ /Error/
+    end
   end
 end
 
