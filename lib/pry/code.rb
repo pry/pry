9e8532149aa877e67661ee1decbc765ad16464f8
John Mair <jrmair@gmail.com>
2012-04-16 12:25:08 +1200

    fixed limitation in rbx melbourne parser
    * complete_expression? on rbx was incorrectly raising for incomplete case statements
    * made incomplete_user_input_exception? privae

--- lib/pry/code.rb
+++ lib/pry/code.rb
@@ -68,12 +68,13 @@ class Pry
         case ex.message
         when /unexpected (\$end|end-of-file|END_OF_FILE)/, # mri, jruby, ironruby
           /unterminated (quoted string|string|regexp) meets end of file/, # "quoted string" is ironruby
-          /missing 'end' for/, /: expecting '[})\]]'$/, /can't find string ".*" anywhere before EOF/, /: expecting keyword_end/ # rbx
+          /missing 'end' for/, /: expecting '[})\]]'$/, /can't find string ".*" anywhere before EOF/, /: expecting keyword_end/, /expecting kWHEN/ # rbx
           true
         else
           false
         end
       end
+      private :incomplete_user_input_exception?
 
       # Retrieve the first complete expression from the passed string.
       #
