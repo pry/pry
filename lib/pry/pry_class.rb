bf4a9d77ba2539cf71f9c8ddef6bfc0a0d70c56f
Yorick Peterse <yorickpeterse@gmail.com>
2012-06-22 10:00:43 +0200

    Use a different ANSI sequences for clearing lines.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/pry/pry_class.rb
+++ lib/pry/pry_class.rb
@@ -138,7 +138,7 @@ class Pry
     # Clear the line before starting Pry. This fixes the issue discussed here:
     # https://github.com/pry/pry/issues/566
     if Pry.config.auto_indent
-      Kernel.print defined?(Win32::Console) ? "\e[0F" : "\e[0A\e[0G"
+      Kernel.print defined?(Win32::Console) ? "\e[0F" : "\e[0G"
     end
 
     # Enter the matrix
