e23eba98b5c280e73dcc53ed58180d965c2924fd
John Mair <jrmair@gmail.com>
2012-06-23 20:14:10 +1200

    added tests for module candidate API

---
+++ test/candidate_helper2.rb
@@ -0,0 +1,8 @@
+# rank 1
+class CandidateTest
+  def test4
+  end
+
+  def test5
+  end
+end
--- test/test_wrapped_module.rb
+++ test/test_wrapped_module.rb
@@ -8,6 +8,91 @@ describe Pry::WrappedModule do
     end
   end
 
+  describe "candidates" do
+    before do
+      class Host
+        source_files = [File.join(File.dirname(__FILE__), "candidate_helper1.rb"),
+                        File.join(File.dirname(__FILE__), "candidate_helper2.rb")]
+
+        source_files.each do |file|
+          binding.eval File.read(file), file, 1
+        end
+
+        # rank 2
+        class CandidateTest
+          def test6
+          end
+        end
+      end
+    end
+
+    describe "number_of_candidates" do
+      it 'should return the correct number of candidates' do
+        Pry::WrappedModule(Host::CandidateTest).number_of_candidates.should == 3
+      end
+    end
+
+    describe "ordering of candidates" do
+      it 'should return class with largest number of methods as primary candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(0).file.should =~ /helper1/
+      end
+
+      it 'should return class with second largest number of methods as second ranked candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(1).file.should =~ /helper2/
+      end
+
+      it 'should return class with third largest number of methods as third ranked candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(2).file.should =~ /#{__FILE__}/
+      end
+
+      it 'should raise when trying to access non-existent candidate' do
+        lambda { Pry::WrappedModule(Host::CandidateTest).candidate(3) }.should.raise Pry::CommandError
+      end
+    end
+
+    describe "source" do
+      it 'should return primary candidates source by default' do
+        wm = Pry::WrappedModule(Host::CandidateTest)
+        wm.source.should == wm.candidate(0).source
+      end
+
+      it 'should return source for highest ranked candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(0).source.should =~ /test1/
+      end
+
+      it 'should return source for second ranked candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(1).source.should =~ /test4/
+      end
+
+      it 'should return source for third ranked candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(2).source.should =~ /test6/
+      end
+    end
+
+    describe "doc" do
+      it 'should return primary candidates doc by default' do
+        wm = Pry::WrappedModule(Host::CandidateTest)
+        wm.doc.should == wm.candidate(0).doc
+      end
+
+      it 'should return doc for highest ranked candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(0).doc.should =~ /rank 0/
+      end
+
+      it 'should return doc for second ranked candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(1).doc.should =~ /rank 1/
+      end
+
+      it 'should return doc for third ranked candidate' do
+        Pry::WrappedModule(Host::CandidateTest).candidate(2).doc.should =~ /rank 2/
+      end
+    end
+
+    after do
+      Object.remove_const(:Host)
+    end
+  end
+
   describe ".method_prefix" do
     before do
       Foo = Class.new
