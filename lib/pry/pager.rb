f65c915a765385d0e449592b88e6a1d13257bd38
Robert Gleeson <rob@flowof.info>
2012-08-08 23:29:51 +0100

    Move 'lesspipe' to Pry::Pager::SystemPager.

--- lib/pry/pager.rb
+++ lib/pry/pager.rb
@@ -3,19 +3,38 @@ class Pry::Pager
     27
   end
 
-  def initialize(text, io)
-    @text = text
-    @out = io
+  def self.page(text, out = $stdout)
+    is_jruby = defined?(RUBY_ENGINE) && RUBY_ENGINE == "jruby"
+    is_jruby ? SimplePager.new(text, out).page : SystemPager.new(text, out).page
   end
 
   def page
-    text_array = @text.lines.to_a
-    text_array.each_slice(Pry::Pager.page_size) do |chunk|
-      @out.puts chunk.join
-      break if chunk.size < Pry::Pager.page_size
-      if text_array.size > Pry::Pager.page_size
-        @out.puts "\n<page break> --- Press enter to continue ( q<enter> to break ) --- <page break>"
-        break if $stdin.gets.chomp == "q"
+    raise NotImplementedError, "#{self.class} does not implement #page."
+  end
+
+  def initialize(text, out = $stdout)
+    @text = text
+    @out = out
+  end
+
+  class SimplePager < Pry::Pager
+    def page
+      text_array = @text.lines.to_a
+      text_array.each_slice(Pry::Pager.page_size) do |chunk|
+        @out.puts chunk.join
+        break if chunk.size < Pry::Pager.page_size
+        if text_array.size > Pry::Pager.page_size
+          @out.puts "\n<page break> --- Press enter to continue ( q<enter> to break ) --- <page break>"
+          break if $stdin.gets.chomp == "q"
+        end
+      end
+    end
+  end
+
+  class SystemPager < Pry::Pager
+    def page
+      IO.popen("less -R -S -F -X", "w") do |less|
+        less.puts @text
       end
     end
   end
