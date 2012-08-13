555ae066447c5090c108d1385f47d09cdd8d042b
Reginald Tan <redge.tan@gmail.com>
2012-08-13 16:21:23 -0400

    show-source on variables/constants that do not respond to source_location will show the source of their class instead

--- test/test_default_commands/test_show_source.rb
+++ test/test_default_commands/test_show_source.rb
@@ -255,6 +255,34 @@ if !mri18_and_no_real_source_location?
 
     end
 
+    describe "on variable or constant" do
+      before do
+        class TestHost
+          def hello
+            "hi there"
+          end
+        end
+      end
+
+      after do
+        Object.remove_const(:TestHost)
+      end
+
+      it "should output source of its class if variable doesn't respond to source_location" do
+        test_host = TestHost.new
+        string = mock_pry(binding,"show-source test_host","exit-all")
+        string.should =~ /class TestHost\n.*def hello/
+      end
+
+      it "should output source of its class if constant doesn't respond to source_location" do
+        TEST_HOST = TestHost.new
+        string = mock_pry(binding,"show-source TEST_HOST","exit-all")
+        string.should =~ /class TestHost\n.*def hello/
+
+        Object.remove_const(:TEST_HOST)
+      end
+    end
+
     describe "on modules" do
       before do
         class ShowSourceTestSuperClass
