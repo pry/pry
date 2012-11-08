667b97e4ebbb0baef4138d68675bca4033424965
Conrad Irwin <conrad.irwin@gmail.com>
2012-11-08 01:18:15 -0800

    Completion for show-source/show-doc/?/$ Array#<tab>

--- lib/pry/helpers/module_introspection_helpers.rb
+++ lib/pry/helpers/module_introspection_helpers.rb
@@ -127,6 +127,22 @@ class Pry
           retry
         end
       end
+
+      def complete(input)
+        if input =~ /([^ ]*)#([a-z0-9_]*)\z/
+          prefix, search = [$1, $2]
+          methods = begin
+                      Pry::Method.all_from_class(binding.eval(prefix))
+                    rescue RescuableException => e
+                      return super
+                    end
+          methods.map do |method|
+            [prefix, method.name].join('#') if method.name.start_with?(search)
+          end.compact
+        else
+          super
+        end
+      end
     end
   end
 end
