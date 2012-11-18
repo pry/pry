20b3fce17d87d764fa148f7272c95ea260153cf7
Conrad Irwin <conrad.irwin@gmail.com>
2012-11-18 01:28:58 -0800

    Fix Indent.screen_size on jruby

--- lib/pry/indent.rb
+++ lib/pry/indent.rb
@@ -410,20 +410,21 @@ class Pry
     # If the window size cannot be determined, return nil.
     def screen_size
       [
-         # io/console adds a winsize method to IO streams.
-         $stdout.tty? && $stdout.respond_to?(:winsize) && $stdout.winsize,
-
          # Some readlines also provides get_screen_size.
+         # Readline comes before IO#winsize because jruby sometimes defaults winsize to [25, 80]
          Readline.respond_to?(:get_screen_size) && Readline.get_screen_size,
+
+         # io/console adds a winsize method to IO streams.
+         # rescue nil for jruby 1.7.0 [jruby/jruby#354]
+         $stdout.tty? && $stdout.respond_to?(:winsize) && ($stdout.winsize rescue nil),
 
          # Otherwise try to use the environment (this may be out of date due
          # to window resizing, but it's better than nothing).
-         [ENV["ROWS"], ENV["COLUMNS"],
+         [ENV["ROWS"], ENV["COLUMNS"]],
 
          # If the user is running within ansicon, then use the screen size
          # that it reports (same caveats apply as with ROWS and COLUMNS)
          ENV['ANSICON'] =~ /\((.*)x(.*)\)/ && [$2, $1]
-        ]
       ].detect do |(_, cols)|
         cols.to_i > 0
       end
