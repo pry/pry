988b27c07109d3c67d67dd0bd6a28c98c00175bd
Ryan Fitzgerald <rwfitzge@gmail.com>
2012-12-23 15:48:20 -0800

    Switch from fibers to threads :broken_heart:

--- spec/helpers/repl_tester.rb
+++ spec/helpers/repl_tester.rb
@@ -1,58 +1,102 @@
 # This is for super-high-level integration testing.
 
-require 'fiber'
+require 'thread'
 
 class ReplTester
   class Input
+    def initialize(tester_mailbox)
+      @tester_mailbox = tester_mailbox
+    end
+
     def readline(prompt)
-      Fiber.yield(prompt)
+      awaken_tester
+      mailbox.pop
+    end
+
+    def mailbox
+      Thread.current[:mailbox]
+    end
+
+    def awaken_tester
+      @tester_mailbox.push nil
     end
   end
 
   def self.start(options = {}, &block)
-    redirect_pry_io Input.new, StringIO.new do
+    Thread.current[:mailbox] = Queue.new
+    instance = nil
+
+    redirect_pry_io Input.new(Thread.current[:mailbox]), StringIO.new do
       instance = new(options)
       instance.instance_eval(&block)
       instance.ensure_exit
     end
+  ensure
+    if instance && instance.thread && instance.thread.alive?
+      instance.thread.kill
+    end
   end
 
-  attr_accessor :pry, :repl, :fiber
+  attr_accessor :thread, :mailbox
 
   def initialize(options = {})
-    @pry   = Pry.new(options)
-    @repl  = Pry::REPL.new(@pry)
+    @pry     = Pry.new(options)
+    @repl    = Pry::REPL.new(@pry)
+    @mailbox = Thread.current[:mailbox]
 
-    @fiber = Fiber.new do
-      @repl.start
+    @thread  = Thread.new do
+      begin
+        Thread.current[:mailbox] = Queue.new
+        @repl.start
+      ensure
+        Thread.current[:session_ended] = true
+        mailbox.push nil
+      end
     end
 
-    @fiber.resume
+    mailbox.pop # wait until the instance reaches its first readline
   end
 
+  # Accept a line of input, as if entered by a user.
   def input(input)
-    Pry.output.send(:initialize) # reset StringIO
-    @fiber.resume(input)
+    reset_output
+    repl_mailbox.push input
+    mailbox.pop # wait until the instance either calls readline or ends
   end
 
+  # Assert that the current prompt matches the given string or regex.
   def prompt(match)
     match.should === @pry.select_prompt
   end
 
+  # Assert that the most recent output (since the last time input was called)
+  # matches the given string or regex.
   def output(match)
     match.should === Pry.output.string.chomp
   end
 
+  # Assert that the Pry session ended naturally after the last input.
+  def assert_exited
+    @should_exit_naturally = true
+  end
+
+  # @private
   def ensure_exit
     if @should_exit_naturally
-      fiber.should.not.be.alive
+      @thread[:session_ended].should.be.true
     else
       input "exit-all"
-      raise "REPL didn't die" if fiber.alive?
+      raise "REPL didn't die" unless @thread[:session_ended]
     end
   end
 
-  def assert_exited
-    @should_exit_naturally = true
+  private
+
+  def reset_output
+    @pry.output = Pry.output = StringIO.new
+  end
+
+  def repl_mailbox
+    @thread[:mailbox]
   end
 end
