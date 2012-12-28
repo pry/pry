0261c8fa36854f9bd403f08a3a6290ba1367b0be
Conrad Irwin <conrad.irwin@gmail.com>
2012-12-28 15:59:41 -0800

    Use correct_indent not auto_indent...
    Prior to this change, when auto_indent was set to true for some tests,
    the green dots would be cleared half-way through.
    
    We should probably re-consider these options, but it seems useful to
    explicitly disable ANSI code rewriting during specs while leaving
    indentation mutating read lines (before this change, there was a bug
    in REPL that did not manifest because auto_indent was false).

--- lib/pry/test/helper.rb
+++ lib/pry/test/helper.rb
@@ -14,7 +14,7 @@ class << Pry
     Pry.config.should_load_plugins = false
     Pry.config.history.should_load = false
     Pry.config.history.should_save = false
-    Pry.config.auto_indent         = false
+    Pry.config.correct_indent      = false
     Pry.config.hooks               = Pry::Hooks.new
     Pry.config.collision_warning   = false
   end
