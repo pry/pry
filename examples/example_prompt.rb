23f4295d53a724c8d4d3c1fa59ca92e757ffd38f
John Mair <jrmair@gmail.com>
2013-01-01 16:39:51 +0100

    removed examples/ subfolder
    I dont think these are helpful to anyone anymore

--- examples/example_prompt.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
-
-# Remember, first prompt in array is the main prompt, second is the wait
-# prompt (used for multiline input when more input is required)
-my_prompt = [ proc { |obj, *| "inside #{obj}> " },
-           proc { |obj, *| "inside #{obj}* "} ]
-
-# Start a Pry session using the prompt defined in my_prompt
-Pry.start(TOPLEVEL_BINDING, :prompt => my_prompt)
