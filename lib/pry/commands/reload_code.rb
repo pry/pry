38e49b16d3098c7bfb997a8f32e59b1c537a93c9
John Mair <jrmair@gmail.com>
2013-01-05 20:31:20 +0100

    renamed reload-method to reload-code and extended functionality
    Should now be able to reload any valid code object, including: methods, modules, commands, procs, etc

---
+++ lib/pry/commands/reload_code.rb
@@ -0,0 +1,35 @@
+class Pry
+  class Command::ReloadCode < Pry::ClassCommand
+    match 'reload-code'
+    group 'Misc'
+    description 'Reload the source file that contains the specified code object'
+
+    def process
+      code_object = Pry::CodeObject.lookup(obj_name, target, _pry_)
+
+      check_for_reloadability(code_object)
+      reload_code_object(code_object)
+    end
+
+    private
+
+    def reload_code_object(code_object)
+      load code_object.source_file
+      output.puts "#{code_object} was reloaded!"
+    end
+
+    def obj_name
+      @obj_name ||= args.empty? ? nil : args.join(" ")
+    end
+
+    def check_for_reloadability(code_object)
+      if !code_object
+        raise CommandError, "Cannot locate #{obj_name}!"
+      elsif !File.exists?(code_object.source_file)
+        raise CommandError, "Cannot reload #{code_object} as it has no associated file on disk. File found was: #{code_object.source_file}"
+      end
+    end
+  end
+
+  Pry::Commands.add_command(Pry::Command::ReloadCode)
+end
