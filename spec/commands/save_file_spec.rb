cf2acbe1c74f079fbf0f1b6460122bde70dd770a
John Mair <jrmair@gmail.com>
2013-01-07 23:12:10 +0100

    Reimplemented play, save-file with Pry::Command::CodeCollector
    Pry::Command::CodeCollector is a mixin that extracts out shared
    functionality from play, save-file and gist. Gist is yet to be
    reimplemented.

--- spec/commands/save_file_spec.rb
+++ spec/commands/save_file_spec.rb
@@ -18,7 +18,7 @@ describe "save-file" do
         f.puts ":cute_horse"
         f.flush
 
-        @t.eval("save-file -f #{path} #{@path}")
+        @t.eval("save-file #{path} --to #{@path}")
 
         File.read(@path).should == File.read(path)
       end
@@ -28,23 +28,23 @@ describe "save-file" do
   describe "-i" do
     it 'should save input expressions to a file (single expression)' do
       @t.eval ':horse_nostrils'
-      @t.eval "save-file -i 1 #{@path}"
+      @t.eval "save-file -i 1 --to #{@path}"
       File.read(@path).should == ":horse_nostrils\n"
     end
 
     it "should display a success message on save" do
       @t.eval ':horse_nostrils'
-      @t.eval("save-file -i 1 #{@path}").should =~ /successfully saved/
+      @t.eval("save-file -i 1 --to #{@path}").should =~ /successfully saved/
     end
 
     it 'should save input expressions to a file (range)' do
       @t.eval ':or_nostrils', ':sucking_up_all_the_oxygen', ':or_whatever'
-      @t.eval "save-file -i 1..2 #{@path}"
+      @t.eval "save-file -i 1..2 --to #{@path}"
       File.read(@path).should == ":or_nostrils\n:sucking_up_all_the_oxygen\n"
     end
   end
 
-  describe "-m" do
+  describe "saving methods" do
     before do
       @o = Object.new
       def @o.baby
@@ -59,16 +59,16 @@ describe "save-file" do
 
     describe "single method" do
       it 'should save a method to a file' do
-        @t.eval "save-file #{@path} -m baby"
+        @t.eval "save-file --to #{@path} baby"
         File.read(@path).should == Pry::Method.from_obj(@o, :baby).source
       end
 
       it "should display a success message on save" do
-        @t.eval("save-file #{@path} -m baby").should =~ /successfully saved/
+        @t.eval("save-file --to #{@path} baby").should =~ /successfully saved/
       end
 
       it 'should save a method to a file truncated by --lines' do
-        @t.eval "save-file #{@path} -m baby --lines 2..4"
+        @t.eval "save-file --to #{@path} baby --lines 2..4"
 
         # must add 1 as first line of method is 1
         File.read(@path).should ==
@@ -76,40 +76,42 @@ describe "save-file" do
       end
     end
 
-    describe "multiple method" do
-      it 'should save multiple methods to a file' do
-        @t.eval "save-file #{@path} -m baby -m bang"
+    # TODO: do we want to reintroduce this spec??
+    #
+    # describe "multiple method" do
+    #   it 'should save multiple methods to a file' do
+    #     @t.eval "save-file #{@path} -m baby -m bang"
 
-        File.read(@path).should == Pry::Method.from_obj(@o, :baby).source +
-          Pry::Method.from_obj(@o, :bang).source
-      end
+    #     File.read(@path).should == Pry::Method.from_obj(@o, :baby).source +
+    #       Pry::Method.from_obj(@o, :bang).source
+    #   end
 
-      it 'should save multiple methods to a file trucated by --lines' do
-        @t.eval "save-file #{@path} -m baby -m bang --lines 2..-2"
+    #   it 'should save multiple methods to a file trucated by --lines' do
+    #     @t.eval "save-file #{@path} -m baby -m bang --lines 2..-2"
 
-        # must add 1 as first line of method is 1
-        File.read(@path).should == (Pry::Method.from_obj(@o, :baby).source +
-          Pry::Method.from_obj(@o, :bang).source).lines.to_a[1..-2].join
-      end
+    #     # must add 1 as first line of method is 1
+    #     File.read(@path).should == (Pry::Method.from_obj(@o, :baby).source +
+    #       Pry::Method.from_obj(@o, :bang).source).lines.to_a[1..-2].join
+    #   end
 
-      it 'should save multiple methods to a file trucated by --lines 1 ' \
-         '(single parameter, not range)' do
-        @t.eval "save-file #{@path} -m baby -m bang --lines 1"
+    #   it 'should save multiple methods to a file trucated by --lines 1 ' \
+    #      '(single parameter, not range)' do
+    #     @t.eval "save-file #{@path} -m baby -m bang --lines 1"
 
-        # must add 1 as first line of method is 1
-        File.read(@path).should == (Pry::Method.from_obj(@o, :baby).source +
-          Pry::Method.from_obj(@o, :bang).source).lines.to_a[0]
-      end
-    end
+    #     # must add 1 as first line of method is 1
+    #     File.read(@path).should == (Pry::Method.from_obj(@o, :baby).source +
+    #       Pry::Method.from_obj(@o, :bang).source).lines.to_a[0]
+    #   end
+    # end
   end
 
   describe "overwrite by default (no --append)" do
     it 'should overwrite specified file with new input' do
       @t.eval ':horse_nostrils'
-      @t.eval "save-file -i 1 #{@path}"
+      @t.eval "save-file -i 1 --to #{@path}"
 
       @t.eval ':sucking_up_all_the_oxygen'
-      @t.eval "save-file -i 2 #{@path}"
+      @t.eval "save-file -i 2 --to #{@path}"
 
       File.read(@path).should == ":sucking_up_all_the_oxygen\n"
     end
@@ -118,48 +120,50 @@ describe "save-file" do
   describe "--append" do
     it 'should append to end of specified file' do
       @t.eval ':horse_nostrils'
-      @t.eval "save-file -i 1 #{@path}"
+      @t.eval "save-file -i 1 --to #{@path}"
 
       @t.eval ':sucking_up_all_the_oxygen'
-      @t.eval "save-file -i 2 #{@path} -a"
+      @t.eval "save-file -i 2 --to #{@path} -a"
 
       File.read(@path).should ==
         ":horse_nostrils\n:sucking_up_all_the_oxygen\n"
     end
   end
 
-  describe "-c" do
+  describe "saving commands" do
     it 'should save a command to a file' do
-      @t.eval "save-file #{@path} -k show-method"
-      cmd = Pry::Method.new(Pry.commands.find_command("show-method").block)
-      File.read(@path).should == Pry::Code.from_method(cmd).to_s
+      @t.eval "save-file --to #{@path} show-source"
+      cmd_source = Pry.commands["show-source"].source
+      File.read(@path).should == cmd_source
     end
   end
 
-  describe "combined options" do
-    before do
-      @o = Object.new
-      def @o.baby
-        :baby
-      end
+  # TODO: reintroduce these specs at some point?
+  #
+  # describe "combined options" do
+  #   before do
+  #     @o = Object.new
+  #     def @o.baby
+  #       :baby
+  #     end
 
-      @t = pry_tester(@o)
-    end
+  #     @t = pry_tester(@o)
+  #   end
 
-    it 'should save input cache and a method to a file (in that order)' do
-      @t.eval ":horse_nostrils"
-      @t.eval "save-file -i 1 -m baby #{@path}"
+  #   it 'should save input cache and a method to a file (in that order)' do
+  #     @t.eval ":horse_nostrils"
+  #     @t.eval "save-file -i 1 -m baby #{@path}"
 
-      File.read(@path).should == ":horse_nostrils\n" +
-        Pry::Method.from_obj(@o, :baby).source
-    end
+  #     File.read(@path).should == ":horse_nostrils\n" +
+  #       Pry::Method.from_obj(@o, :baby).source
+  #   end
 
-    it 'should select a portion to save using --lines' do
-      @t.eval ":horse_nostrils"
-      @t.eval "save-file -i 1 -m baby #{@path} --lines 2..-2"
+  #   it 'should select a portion to save using --lines' do
+  #     @t.eval ":horse_nostrils"
+  #     @t.eval "save-file -i 1 -m baby #{@path} --lines 2..-2"
 
-      str = ":horse_nostrils\n" + Pry::Method.from_obj(@o, :baby).source
-      File.read(@path).should == str.lines.to_a[1..-2].join
-    end
-  end
+  #     str = ":horse_nostrils\n" + Pry::Method.from_obj(@o, :baby).source
+  #     File.read(@path).should == str.lines.to_a[1..-2].join
+  #   end
+  # end
 end
