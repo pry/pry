0c3ea9a6c5ce27ef19f961a8d9d11a95e365723d
John Mair <jrmair@gmail.com>
2013-01-09 01:39:02 +0100

    Pry::Command::CodeCollector: replace module inclusion with delegation
    And implement play and save-file using the new delegator. 'gist' still needs
    to be re-implemented using CodeCollector.
    
    ************
    TODO:
    **************
    Couldn't write a test for `play --lines 4..5` with implied target.eval("__FILE__") parameter,
    need to come up with a decent test.

--- lib/pry/commands/play.rb
+++ lib/pry/commands/play.rb
@@ -1,7 +1,5 @@
 class Pry
   class Command::Play < Pry::ClassCommand
-    include Command::CodeCollector
-
     match 'play'
     group 'Editing'
     description 'Play back a string variable or a method or a file as input.'
@@ -22,11 +20,14 @@ class Pry
     BANNER
 
     def options(opt)
-      super
+      CodeCollector.inject_options(opt)
+
       opt.on :open, "open", 'When used with the -m switch, it plays the entire method except the last line, leaving the method definition "open". `amend-line` can then be used to modify the method.'
     end
 
     def process
+      @cc = CodeCollector.new(args, opts, _pry_)
+
       perform_play
       run "show-input" unless Pry::Code.complete_expression?(eval_string)
     end
@@ -35,8 +36,26 @@ class Pry
       eval_string << (opts.present?(:open) ? restrict_to_lines(content, (0..-2)) : content)
     end
 
-    def no_arg
-      _pry_.last_file
+    def content
+      if args.first
+        @cc.content
+      else
+        file_content
+      end
+    end
+
+    # The file to play from when no code object is specified.
+    # e.g `play --lines 4..10`
+    def default_file
+      target.eval("__FILE__") && File.expand_path(target.eval("__FILE__"))
+    end
+
+    def file_content
+      if default_file && File.exists?(default_file)
+        @cc.restrict_to_lines(File.read(default_file), @cc.line_range)
+      else
+        raise CommandError, "File does not exist! File was: #{default_file.inspect}"
+      end
     end
   end
 
