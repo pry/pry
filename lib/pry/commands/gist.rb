b6b78157a5b502a2eaa0b997e98184dc0547895b
John Mair <jrmair@gmail.com>
2013-01-15 20:08:32 +0100

    gist: Use Pry::CodeCollector (like play/save-file)
    This enables a simplified UI, i.e: gist my_file.rb instead of gist -f my_file.rb
    and `gist my_method` instead of `gist -m my_method`. Unfortunately, we had to kill our
    tests in the process of doing this (gist_spec.rb is just commented out) - however since play
    and save-file have tests and they use the same Pry::CodeCollector object 'gist' is indirectly
    tested. Nonetheless, we should re-add tests at some point

--- lib/pry/commands/gist.rb
+++ lib/pry/commands/gist.rb
@@ -1,182 +1,69 @@
 class Pry
-  module Gist
-    DESCRIPTION = 'Upload code, docs, history to https://gist.github.com/'
-    INVOCATIONS = {
-      :method   => ['gist -m my_method', 'gist the method my_method' ],
-      :doc      => ['gist -d my_method', 'gist the docs for my_method' ],
-      :input    => ['gist -i 1..2', 'gist the input expressions from 1 to 2' ],
-      :kommand  => ['gist -k show-method', 'gists pry command show-method' ],
-      :class    => ['gist -c Pry::Method', 'gist the Pry::Method class' ],
-      :jist     => ['jist -c Pry::Method', 'alias for the above' ],
-      :lines    => ['gist -m my_method --lines 2..-2', 'limit by range' ],
-      :cliponly => ['gist -m my_method --clip', 'copy (but do not gist)' ],
-      :clipit   => ['clipit -m my_method', 'alias for the above' ],
-      # TODO:
-      # :var      =>
-      # :hist     =>
-      # :file     =>
-    }
-    class << self
-      def example_code sym; INVOCATIONS[sym][0] end
-      def example_description sym; INVOCATIONS[sym][1] end
-      def examples_docs
-        INVOCATIONS.keys.map do |e|
-          "e.g.: %-33s # %s " % [example_code(e), example_description(e)]
-        end.join "\n"
-      end
-      def require_jist; require 'jist' end
-    end
-  end
-
   class Command::Gist < Pry::ClassCommand
-    include Pry::Helpers::DocumentationHelpers
-
     match 'gist'
     group 'Misc'
-    description Pry::Gist::DESCRIPTION
-    command_options :requires_gem => 'jist', :shellwords => false
+    description 'Playback a string variable or a method or a file as input.'
 
-banner <<BANNER
-Usage: gist [options]
+    banner <<-'BANNER'
+      Usage: gist [OPTIONS] [--help]
 
-#{Pry::Gist::DESCRIPTION}
+      The gist command enables you to gist code from files and methods to github.
 
-If you would like to associate your gists with your GitHub account, run `gist --login`.
-
-#{Pry::Gist.examples_docs}
-BANNER
-
-    attr_accessor :content, :filename
+      gist -i 20 --lines 1..3
+      gist Pry#repl --lines 1..-1
+      gist Rakefile --lines 5
+    BANNER
 
     def setup
-      Pry::Gist.require_jist # extracted so test can stub out
-      @content = ''
-    end
-
-    def from_pry_api api_obj
-      @content << api_obj.source << "\n"
-      @filename = api_obj.source_file
+      require 'jist'
     end
 
     def options(opt)
-      ext ='ruby'
+      CodeCollector.inject_options(opt)
       opt.on :login, "Authenticate the jist gem with GitHub"
-      opt.on :d, :doc, "Gist a method's documentation", :argument => true do |meth_name|
-        meth = get_method_or_raise(meth_name, target, {})
-        text.no_color do
-          @content << process_comment_markup(meth.doc) << "\n"
-        end
-        @filename = meth.source_file + ".doc"
-      end
-      opt.on :m, :method, "Gist a method's source", :argument => true do |meth_name|
-        from_pry_api get_method_or_raise(meth_name, target, {})
-      end
-      opt.on :k, :command, "Gist a command's source", :argument => true do |command_name|
-        command = find_command(command_name)
-        from_pry_api Pry::Method.new(command.block)
-      end
-      opt.on :c, :class, "Gist a class or module's source", :argument => true do |class_name|
-        from_pry_api Pry::WrappedModule.from_str(class_name, target)
-      end
-      opt.on :var, "Gist a variable's content", :argument => true do |variable_name|
-        begin
-          obj = target.eval(variable_name)
-        rescue Pry::RescuableException
-          raise CommandError, "Gist failed: Invalid variable name: #{variable_name}"
-        end
-
-        @content << Pry.config.gist.inspecter.call(obj) << "\n"
-      end
-      opt.on :hist, "Gist a range of Readline history lines",  :optional_argument => true, :as => Range, :default => -20..-1 do |range|
-        h = Pry.history.to_a
-        @content << h[one_index_range(convert_to_range(range))].join("\n") << "\n"
-      end
-
-      opt.on :f, :file, "Gist a file.", :argument => true do |file|
-        @content << File.read(File.expand_path(file)) << "\n"
-        @filename = file
-      end
-      opt.on :o, :out, "Gist entries from Pry's output result history. Takes an index or range", :optional_argument => true,
-      :as => Range, :default => -1 do |range|
-        range = convert_to_range(range)
-
-        range.each do |v|
-          @content << Pry.config.gist.inspecter.call(_pry_.output_array[v])
-        end
-
-        @content << "\n"
-      end
-      opt.on :clip, "Copy the selected content to clipboard instead, do NOT gist it", :default => false
       opt.on :p, :public, "Create a public gist (default: false)", :default => false
-      opt.on :l, :lines, "Only gist a subset of lines from the gistable content", :optional_argument => true, :as => Range, :default => 1..-1
-      opt.on :i, :in, "Gist entries from Pry's input expression history. Takes an index or range", :optional_argument => true,
-      :as => Range, :default => -1 do |range|
-        range = convert_to_range(range)
-        input_expressions = _pry_.input_array[range] || []
-        Array(input_expressions).each_with_index do |code, index|
-          corrected_index = index + range.first
-          if code && code != ""
-            @content << code
-            if code !~ /;\Z/
-              @content << "#{comment_expression_result_for_gist(Pry.config.gist.inspecter.call(_pry_.output_array[corrected_index]))}"
-            end
-          end
-        end
-      end
+      opt.on :clip, "Copy the selected content to clipboard instead, do NOT gist it", :default => false
     end
 
     def process
       return Jist.login! if opts.present?(:login)
+      cc = CodeCollector.new(args, opts, _pry_)
 
-      if @content =~ /\A\s*\z/
+      if cc.content =~ /\A\s*\z/
         raise CommandError, "Found no code to gist."
       end
 
       if opts.present?(:clip)
-        perform_clipboard
+        clipboard_content(cc.content)
       else
-        perform_gist
+        # we're overriding the default behavior of the 'in' option (as
+        # defined on CodeCollector) with our local behaviour.
+        content = opts.present?(:in) ? input_content : cc.content
+        gist_content content, cc.file
       end
     end
 
-    # copy content to clipboard instead (only used with --clip flag)
-    def perform_clipboard
-      Jist.copy(@content)
+    def clipboard_content(content)
+      Jist.copy(content)
       output.puts "Copied content to clipboard!"
     end
 
-    def perform_gist
-      if opts.present?(:lines)
-        @content = restrict_to_lines(content, opts[:l])
+    def input_content
+      content = ""
+      CodeCollector.input_expression_ranges.each do |range|
+        input_expressions = _pry_.input_array[range] || []
+        Array(input_expressions).each_with_index do |code, index|
+          corrected_index = index + range.first
+          if code && code != ""
+            content << code
+            if code !~ /;\Z/
+              content << "#{comment_expression_result_for_gist(Pry.config.gist.inspecter.call(_pry_.output_array[corrected_index]))}"
+            end
+          end
+        end
       end
 
-      response = Jist.gist(content, :filename => filename_or_fake,
-                                    :public => !!opts[:p])
-
-      if response
-        url = response['html_url']
-        Jist.copy(url)
-        output.puts 'Gist created at URL, which is now in the clipboard: ', url
-      end
-    end
-
-    def filename_or_fake
-      case @filename
-      when nil
-        'anonymous.rb' # not sure what triggers this condition
-      when '(pry)'
-        'repl.rb'
-      else
-        File.basename(@filename)
-      end
-    end
-
-    def convert_to_range(n)
-      if !n.is_a?(Range)
-        (n..n)
-      else
-        n
-      end
+      content
     end
 
     def comment_expression_result_for_gist(result)
@@ -188,9 +75,18 @@ BANNER
           content << "#    #{line}"
         end
       end
+
       content
     end
 
+    def gist_content(content, filename)
+      response = Jist.gist(content, :filename => filename || "pry_gist.rb", :public => !!opts[:p])
+      if response
+        url = response['html_url']
+        Jist.copy(url)
+        output.puts 'Gist created at URL, which is now in the clipboard: ', url
+      end
+    end
   end
 
   Pry::Commands.add_command(Pry::Command::Gist)
