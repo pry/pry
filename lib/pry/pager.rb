80ddbcc3d6800cde2c2155538fd025378ea78ec7
☈king <rking@sharpsaw.org>
2013-01-30 21:14:06 -0600

    Tweak and Refactor Pry::Terminal
    The biggest behavior change is to favor `io/console` (but still not when
    on jruby). The previous favorite was `Readline.get_screen_size`, but
    that thing is stale just like the env. The only reason it gets updated
    is if you put `Pry.auto_resize!` in your `~/.pryrc`
    
    The next change is more error handling in `Pry.auto_resize!`
    
    Otherwise I renamed `TerminalInfo` to `Terminal`, which is something
    andrewvos and I discussed way back when `TerminalInfo` first came about.
    
    The rest was a little Extract Method stuff, no biggie.

--- lib/pry/pager.rb
+++ lib/pry/pager.rb
@@ -27,7 +27,7 @@ class Pry::Pager
   end
 
   def self.page_size
-    rows = Pry::TerminalInfo.screen_size
+    rows = Pry::Terminal.screen_size
     @page_size ||= (rows && rows.first || 27)
   end
 
