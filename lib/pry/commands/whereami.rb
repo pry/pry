1c746565d1e7e920d4d917071484c7164a869fd3
John Mair <jrmair@gmail.com>
2013-02-02 18:02:05 +0100

    Add -m, -c, -f options to whereami command.
    -m, the current method in its totality
    -c, the current class/module that is the current 'candidate' (monkeypatch)
    -f, the entire file

--- lib/pry/commands/whereami.rb
+++ lib/pry/commands/whereami.rb
@@ -1,16 +1,23 @@
 class Pry
   class Command::Whereami < Pry::ClassCommand
+
+    class << self
+      attr_accessor :method_size_cutoff
+    end
+
+    @method_size_cutoff = 30
+
     match 'whereami'
     description 'Show code surrounding the current context.'
     group 'Context'
 
     banner <<-'BANNER'
-      Usage: whereami [-qn] [N]
+      Usage: whereami [-qn] [LINES]
 
       Describe the current location. If you use `binding.pry` inside a method then
       whereami will print out the source for that method.
 
-      If a number is passed, then N lines before and after the current line will be
+      If a number is passed, then LINES lines before and after the current line will be
       shown instead of the method itself.
 
       The `-q` flag can be used to suppress error messages in the case that there's
@@ -25,22 +32,42 @@ class Pry
     BANNER
 
     def setup
-      @method = Pry::Method.from_binding(target)
-      @file = target.eval('__FILE__')
+      @file = expand_path(target.eval('__FILE__'))
       @line = target.eval('__LINE__')
+      @method = Pry::Method.from_binding(target)
     end
 
     def options(opt)
       opt.on :q, :quiet,             "Don't display anything in case of an error"
       opt.on :n, :"no-line-numbers", "Do not display line numbers"
+      opt.on :m, :"method", "Show the complete source for the current method."
+      opt.on :c, :"class", "Show the complete source for the current class or module."
+      opt.on :f, :"file", "Show the complete source for the current file."
     end
 
     def code
-      @code ||= if show_method?
-                  Pry::Code.from_method(@method)
+      @code ||= if opts.present?(:m)
+                  method_code or raise CommandError, "Cannot find method code."
+                elsif opts.present?(:c)
+                  class_code or raise CommandError, "Cannot find class code."
+                elsif opts.present?(:f)
+                  Pry::Code.from_file(@file)
+                elsif args.any?
+                  code_window
                 else
-                  Pry::Code.from_file(@file).around(@line, window_size)
+                  default_code
                 end
+    end
+
+    def code?
+      !!code
+    rescue MethodSource::SourceNotFoundError
+      false
+    end
+
+    def bad_option_combination?
+      [opts.present?(:m), opts.present?(:f),
+       opts.present?(:c), args.any?].count(true) > 1
     end
 
     def location
@@ -48,6 +75,10 @@ class Pry
     end
 
     def process
+      if bad_option_combination?
+        raise CommandError, "Only one of -m, -c, -f, and  LINES may be specified."
+      end
+
       if nothing_to_do?
         return
       elsif internal_binding?(target)
@@ -57,9 +88,10 @@ class Pry
 
       set_file_and_dir_locals(@file)
 
-      output.puts "\n#{text.bold('From:')} #{location}:\n\n"
-      output.puts code.with_line_numbers(use_line_numbers?).with_marker(marker)
-      output.puts
+      out = "\n#{text.bold('From:')} #{location}:\n\n" +
+        code.with_line_numbers(use_line_numbers?).with_marker(marker) + "\n"
+
+      stagger_output(out)
     end
 
     private
@@ -88,17 +120,53 @@ class Pry
       end
     end
 
-    def show_method?
-      args.empty? && @method && @method.source? && @method.source_range.count < 20 &&
-      # These checks are needed in case of an eval with a binding and file/line
-      # numbers set to outside the function. As in rails' use of ERB.
-      @method.source_file == @file && @method.source_range.include?(@line)
+    def small_method?
+      @method.source_range.count < self.class.method_size_cutoff
     end
 
-    def code?
-      !!code
-    rescue MethodSource::SourceNotFoundError
-      false
+    def default_code
+      if method_code && small_method?
+        method_code
+      else
+        code_window
+      end
+    end
+
+    def code_window
+      Pry::Code.from_file(@file).around(@line, window_size)
+    end
+
+    def method_code
+      return @method_code if @method_code
+
+      if valid_method?
+        @method_code = Pry::Code.from_method(@method)
+      end
+    end
+
+    def class_code
+      return @class_code if @class_code
+
+      if valid_method?
+        mod = Pry::WrappedModule(@method.owner)
+        idx = mod.candidates.find_index { |v| expand_path(v.source_file) == @file }
+        @class_code = idx && Pry::Code.from_module(mod, idx)
+      end
+    end
+
+    def valid_method?
+      @method && @method.source? && expand_path(@method.source_file) == @file &&
+        @method.source_range.include?(@line)
+    end
+
+    def expand_path(f)
+      return if !f
+
+      if Pry.eval_path == f
+        f
+      else
+        File.expand_path(f)
+      end
     end
 
     def window_size
