7c8a849fef142cf354095f79c024cc2b8cfcd568
Kyrylo Silin <kyrylosilin@gmail.com>
2013-02-02 17:27:34 +0200

    Refactor the way commands are required
    Fix issue #834 (pry-0.9.11.4 failing on Windows 7 64 bit - cannot load
    such file)
    
    The problem was introduced by this commit:
    25cbe4313729d53cd8254dca8e3ce0022ab499eb
    
    This bug affects *only* Ruby 1.9.3p327, on Windows.
    
                Choose your destiny
                        |
        --N00B-----------------------PRO--
        |                                |
      TL;DR:                http://bugs.ruby-lang.org/issues/7374
      File::expand_path
      is bugged on
      Ruby 1.9.3p327
    
    This fix is simply an ugly workaround.

--- lib/pry/commands.rb
+++ lib/pry/commands.rb
@@ -1,6 +1,6 @@
 # Default commands used by Pry.
 Pry::Commands = Pry::CommandSet.new
 
-Dir[File.expand_path('../commands/*.rb', __FILE__)].each do |file|
+Dir[File.expand_path('../commands', __FILE__) + '/*.rb'].each do |file|
   require file
 end
