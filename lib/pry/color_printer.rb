pry/pry

commit d703320e38397c1b3bf9cb3fc4389f9c741c448b

Fix inspect fallback for weird objects

lib/pry/color_printer.rb
@@ -24,10 +24,29 @@ class Pry
       elsif str.include?("\e[")
         ["#{str}\e[0m", width]
       elsif str.start_with?('#<') || str == '=' || str == '>'
-        ["#{OBJ_COLOR}#{str}\e[0m", width]
+        [highlight_object_literal(str), width]
       else
         [CodeRay.scan(str, :ruby).term, width]
       end
+    end
+
+    def pp(obj)
+      super
+    rescue
+      # Read the class name off of the singleton class to provide a default
+      # inspect.
+      eig    = class << obj; self; end
+      klass  = Pry::Method.safe_send(eig, :ancestors).first
+      obj_id = obj.__id__.to_s(16) rescue 0
+      str    = "#<#{klass}:0x#{obj_id}>"
+
+      text(Pry.color ? highlight_object_literal(str) : str)
+    end
+
+    private
+
+    def highlight_object_literal(object_literal)
+      "#{OBJ_COLOR}#{object_literal}\e[0m"
     end
   end
 end

commit 42c3b6719d780420a6d2309cd47997d204e08965

Fix a couple of SimplePager bugs
* Exiting with q was broken by d703320e38397c1b3bf9cb3fc4389f9c741c448b.

* We were only removing color codes in the case where the input string
  ends with a newline, which means we were overestimating the length of
  strings that don't end with newlines. After fixing that, SimplePager
  can accurately page through the inspect output for _pry_.

lib/pry/color_printer.rb
@@ -32,7 +32,9 @@ class Pry
 
     def pp(obj)
       super
-    rescue
+    rescue => e
+      raise if e.is_a? Pry::Pager::StopPaging
+
       # Read the class name off of the singleton class to provide a default
       # inspect.
       eig    = class << obj; self; end

lib/pry/pager.rb
@@ -181,7 +181,7 @@ class Pry::Pager
           @row += ((@col + line_length(line) - 1) / @cols) + 1
           @col  = 0
         else
-          @col += line.length
+          @col += line_length(line)
         end
       end
     end