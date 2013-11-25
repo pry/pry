pry/pry

commit 16c08857a69b772775c31f325b482637fdf9d48e

Append to Strings with #<< rather than #+.

lib/pry/command_set.rb
@@ -410,7 +410,7 @@ class Pry
       else
         commands.keys.select do |key|
           String === key && key.start_with?(search)
-        end.map{ |key| key + " " } + Bond::DefaultMission.completions
+        end.map{ |key| key + " " } << Bond::DefaultMission.completions
       end
     end
   end

lib/pry/cli.rb
@@ -114,7 +114,7 @@ Copyright (c) 2013 John Mair (banisterfiend)
 --
 }
   on :e, :exec=, "A line of code to execute in context before the session starts" do |input|
-    exec_string << input + "\n"
+    exec_string << input << "\n"
   end
 
   on "no-pager", "Disable pager for long output" do
@@ -155,7 +155,7 @@ Copyright (c) 2013 John Mair (banisterfiend)
     puts "Installed Plugins:"
     puts "--"
     Pry.locate_plugins.each do |plugin|
-      puts "#{plugin.name}".ljust(18) + plugin.spec.summary
+      puts "#{plugin.name}".ljust(18) << plugin.spec.summary
     end
     exit
   end

lib/pry/code.rb
@@ -356,7 +356,7 @@ class Pry
     #
     # @return [String]
     def raw
-      @lines.map(&:line).join("\n") + "\n"
+      @lines.map(&:line).join("\n") << "\n"
     end
 
     # Return the number of lines stored.

lib/pry/command.rb
@@ -307,8 +307,7 @@ class Pry
       collision_type ||= 'local-variable' if arg_string.match(%r{\A\s*[-+*/%&|^]*=})
 
       if collision_type
-        output.puts "#{text.bold('WARNING:')} Calling Pry command '#{command_match}'," +
-                                                          "which conflicts with a #{collision_type}.\n\n"
+        output.puts "#{text.bold('WARNING:')} Calling Pry command '#{command_match}', which conflicts with a #{collision_type}.\n\n"
       end
     rescue Pry::RescuableException
     end

lib/pry/commands.rb
@@ -1,6 +1,6 @@
 # Default commands used by Pry.
 Pry::Commands = Pry::CommandSet.new
 
-Dir[File.expand_path('../commands', __FILE__) + '/*.rb'].each do |file|
+Dir[File.expand_path('../commands', __FILE__) << '/*.rb'].each do |file|
   require file
 end

lib/pry/commands/amend_line.rb
@@ -48,11 +48,11 @@ class Pry
 
     def insert_into_array(array, range)
       insert_slot = Array(range).first
-      array.insert(insert_slot, arg_string[1..-1] + "\n")
+      array.insert(insert_slot, arg_string[1..-1] << "\n")
     end
 
     def replace_in_array(array, range)
-      array[range] = arg_string + "\n"
+      array[range] = arg_string << "\n"
     end
 
     # @return [Fixnum] The number of lines currently in `eval_string` (the input buffer).

lib/pry/commands/find_method.rb
@@ -97,7 +97,7 @@ class Pry
       output.puts text.bold(klass.name)
       grouped[klass].each do |method|
         header = method.name_with_owner
-        output.puts header + additional_info(header, method)
+        output.puts header << additional_info(header, method)
       end
     end
 
@@ -105,7 +105,7 @@ class Pry
     # if `-c` was not given
     def additional_info(header, method)
       if opts.content?
-        ": " + colorize_code(matched_method_lines(header, method))
+        ": " << colorize_code(matched_method_lines(header, method))
       else
         ""
       end

lib/pry/commands/help.rb
@@ -59,7 +59,7 @@ class Pry
     # @param [Array<Pry::Command>]] commands
     # @return [String] The generated help string.
     def help_text_for_commands(name, commands)
-      "#{text.bold(name)}\n" + commands.map do |command|
+      "#{text.bold(name)}\n" << commands.map do |command|
         "  #{command.options[:listing].to_s.ljust(18)} #{command.description}"
       end.join("\n")
     end

commit 40321e1bd9af24b19b626bebd5e3ca747b01bdba

lib/pry/command_set.rb: do not mutate completions (fixes #1021)
The bug was introduced by 16c08857a69b772775c31f325b482637fdf9d48e

lib/pry/command_set.rb
@@ -410,7 +410,7 @@ class Pry
       else
         commands.keys.select do |key|
           String === key && key.start_with?(search)
-        end.map{ |key| key + " " } << Bond::DefaultMission.completions
+        end.map{ |key| key + " " } + Bond::DefaultMission.completions
       end
     end
   end