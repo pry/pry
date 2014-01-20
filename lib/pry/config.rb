dbd11a287ffcd2e850746d375ad0326cbefc8a3f
Robert Gleeson <therobot.johnny5@gmail.com>
2014-01-20 23:02:23 +0100

    support old Pry#hooks= API (argument as a Hash) on Pry::Config.

--- lib/pry/config.rb
+++ lib/pry/config.rb
@@ -58,6 +58,20 @@ class Pry::Config
     @lookup
   end
 
+  #
+  # FIXME
+  # @param [Pry::Hooks] hooks
+  #
+  def hooks=(hooks)
+    if hooks.is_a?(Hash)
+      warn "Hash-based hooks are now deprecated! Use a `Pry::Hooks` object " \
+           "instead! http://rubydoc.info/github/pry/pry/master/Pry/Hooks"
+      self["hooks"] = Pry::Hooks.from_hash(hooks)
+    else
+      self["hooks"] = hooks
+    end
+  end
+
   def quiet?
     quiet
   end
