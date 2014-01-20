3093246eb3d8f976a2d929786009bff8f8cb2b48
Robert Gleeson <therobot.johnny5@gmail.com>
2014-01-20 09:29:37 +0100

    define Ls/Gist/History defaults on instance of Pry::Config::Default.

--- lib/pry/config/default.rb
+++ lib/pry/config/default.rb
@@ -44,7 +44,54 @@ class Pry::Config::Default < Pry::Config
 
   def initialize(*)
     super(nil)
+    configure_ls
+    configure_gist
+    configure_history
   end
 
   state.each { |key, value| define_method(key, &value) }
+
+private
+  # TODO:
+  # all of this configure_* stuff is a relic of old code.
+  # we should try move this code to being command-local.
+  def configure_ls
+    @lookup["ls"] = OpenStruct.new({
+      :heading_color            => :bright_blue,
+      :public_method_color      => :default,
+      :private_method_color     => :blue,
+      :protected_method_color   => :blue,
+      :method_missing_color     => :bright_red,
+      :local_var_color          => :yellow,
+      :pry_var_color            => :default,     # e.g. _, _pry_, _file_
+      :instance_var_color       => :blue,        # e.g. @foo
+      :class_var_color          => :bright_blue, # e.g. @@foo
+      :global_var_color         => :default,     # e.g. $CODERAY_DEBUG, $eventmachine_library
+      :builtin_global_color     => :cyan,        # e.g. $stdin, $-w, $PID
+      :pseudo_global_color      => :cyan,        # e.g. $~, $1..$9, $LAST_MATCH_INFO
+      :constant_color           => :default,     # e.g. VERSION, ARGF
+      :class_constant_color     => :blue,        # e.g. Object, Kernel
+      :exception_constant_color => :magenta,     # e.g. Exception, RuntimeError
+      :unloaded_constant_color  => :yellow,      # Any constant that is still in .autoload? state
+      :separator                => "  ",
+      :ceiling                  => [Object, Module, Class]
+    })
+  end
+
+  def configure_gist
+    @lookup["gist"] = OpenStruct.new
+    gist.inspecter = proc(&:pretty_inspect)
+  end
+
+  def configure_history
+    @lookup["history"] = OpenStruct.new
+    history.should_save = true
+    history.should_load = true
+    history.file = File.expand_path("~/.pry_history") rescue nil
+    if history.file.nil?
+      self.should_load_rc = false
+      history.should_save = false
+      history.should_load = false
+    end
+  end
 end
