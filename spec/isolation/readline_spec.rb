b4d805f63b5c4837e9e68d9bcf793a971dda0612
Robert Gleeson <rob@flowof.info>
2014-02-03 13:46:51 +0100

    confirm 'Readline' is made available on Pry.start().

--- spec/isolation/readline_spec.rb
+++ spec/isolation/readline_spec.rb
@@ -7,4 +7,11 @@ describe "Readline" do
       defined?(Readline).should.be.nil
     end
   end
+
+  describe "on invoke of 'pry'" do
+    it "is made available" do
+      Pry.start self, input: StringIO.new("exit-all\n"), output: StringIO.new
+      defined?(Readline).should == "constant"
+    end
+  end
 end
