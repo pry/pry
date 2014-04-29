df4a3b48af85e53fe5ec0373d228e647ace62006
Ryan Fitzgerald <rwfitzge@gmail.com>
2014-04-28 23:00:04 -0700

    Verify that setting Pry.input avoids loading Readline
    It's kind of unfortunate that just passing the `input` option doesn't
    also do this, but it's not obvious how to fix it without major
    refactoring of the command history.

--- spec/regression/readline_spec.rb
+++ spec/regression/readline_spec.rb
@@ -21,9 +21,19 @@ describe "Readline" do
   it "is loaded on invoking 'pry'" do
     code = <<-RUBY
       require "pry"
-      Pry.start self, input: StringIO.new("exit-all\n"), output: StringIO.new
+      Pry.start self, input: StringIO.new("exit-all"), output: StringIO.new
       puts defined?(Readline)
     RUBY
     `#@ruby -I #@pry_dir -e '#{code}'`.end_with?("constant\n").should == true
+  end
+
+  it "is not loaded on invoking 'pry' if Pry.input is set" do
+    code = <<-RUBY
+      require "pry"
+      Pry.input = StringIO.new("exit-all")
+      Pry.start self, output: StringIO.new
+      p defined?(Readline)
+    RUBY
+    `#@ruby -I #@pry_dir -e '#{code}'`.end_with?("nil\n").should == true
   end
 end
