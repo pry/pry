ed58b9716a555812a5f2c3e00ff63309e2efbb6b
Conrad Irwin <conrad.irwin@gmail.com>
2014-05-07 00:59:36 -0700

    Fix reload-code specs [Fixes #1224]
    The cascading problem was caused by reloading sensitive bits of bacon

--- spec/commands/reload_code_spec.rb
+++ spec/commands/reload_code_spec.rb
@@ -4,9 +4,10 @@ describe "reload_code" do
   describe "reload_current_file" do
     it 'raises an error source code not found' do
       proc do
-        pry_eval(
-          "reload-code")
-      end.should.raise(Pry::CommandError).message.should =~ /cannot be found on disk!/
+        eval <<-RUBY, TOPLEVEL_BINDING, 'does_not_exist.rb', 1
+          pry_eval(binding, "reload-code")
+        RUBY
+      end.should.raise(Pry::CommandError)
     end
 
     it 'raises an error when class not found' do
@@ -14,7 +15,7 @@ describe "reload_code" do
         pry_eval(
           "cd Class.new(Class.new{ def goo; end; public :goo })",
           "reload-code")
-      end.should.raise(Pry::CommandError).message.should =~ /Cannot locate self/
+      end.should.raise(Pry::CommandError)
     end
   end
 end
