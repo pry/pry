pry/pry

commit a293f1f3aa1f44b8ed74951a1b4b30670fb76774

helpers/text: respect RL_PROMPT_START_IGNORE and RL_PROMPT_END_IGNORE
Addresses to #493. Fixes the described bug in the situation when you use
helper methods in your Pry prompt like this:

  Pry.prompt = [
    proc { |_, _, _| Pry::Helpers::Text.red("YO > ") },
    proc { |_, _, _| "| "}
  ]

lib/pry/helpers/text.rb
@@ -18,11 +18,11 @@ class Pry
 
       COLORS.each_pair do |color, value|
         define_method color do |text|
-          "\033[0;#{30+value}m#{text}\033[0m"
+          "\001\033[0;#{30+value}m\002#{text}\001\033[0m\002"
         end
 
         define_method "bright_#{color}" do |text|
-          "\033[1;#{30+value}m#{text}\033[0m"
+          "\001\033[1;#{30+value}m\002#{text}\001\033[0m\002"
         end
       end
 
@@ -98,4 +98,3 @@ class Pry
     end
   end
 end
-

commit c220c40892021b4a8dc6539b705c3c3d2cb231ac

helpers/text: when strip colours, also strip special characters
Fixes build failures introduced in

  a293f1f3aa1f44b8ed74951a1b4b30670fb76774

lib/pry/helpers/text.rb
@@ -31,7 +31,7 @@ class Pry
       # @param  [String, #to_s] text
       # @return [String] _text_ stripped of any color codes.
       def strip_color(text)
-        text.to_s.gsub(/\e\[.*?(\d)+m/ , '')
+        text.to_s.gsub(/(\001)?\e\[.*?(\d)+m(\002)?/ , '')
       end
 
       # Returns _text_ as bold text for use on a terminal.