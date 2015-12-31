6531140f068f639f17b385d9671e36b836643adc
strcmp <28xdb6+fvutuvy4f@grr.la>
2015-12-31 15:25:33 -0800

    add tests

--- spec/config/lazy_spec.rb
+++ spec/config/lazy_spec.rb
@@ -1,13 +1,22 @@
 require 'helper'
 RSpec.describe Pry::Config::Lazy do
-  let(:lazyobject) do
+  let(:lazyobj) do
     Class.new do
       include Pry::Config::Lazy
-      lazy_implement({foo: proc {"bar"}})
+      lazy_implement({foo: proc {"foo"}, bar: proc {"bar"}})
     end.new
   end
 
-  it 'memorizes value after first call' do
-    expect(lazyobject.foo).to equal(lazyobject.foo)
+  describe "on call of a lazy method" do
+    it "memoizes the return value" do
+      expect(lazyobj.foo).to be(lazyobj.foo)
+    end
+  end
+
+  describe "#lazy_keys" do
+    it "tracks a list of lazy keys" do
+      lazyobj.foo # at least one lazy method has to be called before #lazy_keys could return a non-empty array.
+      expect(lazyobj.lazy_keys).to eq([:foo, :bar])
+    end
   end
 end
