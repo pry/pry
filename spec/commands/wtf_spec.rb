30cca64e5a75d1efb62b558c018d7ab44492b02a
Aleksandar Kostadinov <akostadi@redhat.com>
2016-03-01 23:59:30 +0200

    commands/wtf,pry: add support for Exception#cause
    Fixes #1449 (support exceptions with cause)
    Replaces #1525

---
+++ spec/commands/wtf_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../helper'
+
+describe "wtf?!" do
+  let(:tester) do
+    pry_tester do
+      def last_exception=(ex)
+        @pry.last_exception = ex
+      end
+
+      def last_exception
+        @pry.last_exception
+      end
+    end
+  end
+
+  it "unwinds nested exceptions" do
+    if Gem::Version.new(RUBY_VERSION) <= Gem::Version.new('2.0.0')
+      skip('Exception#cause is not supported')
+    end
+
+    begin
+      begin
+        begin
+          raise 'inner'
+        rescue RuntimeError
+          raise 'outer'
+        end
+      end
+    rescue RuntimeError => ex
+      tester.last_exception = ex
+    end
+
+    expect(tester.eval('wtf -v')).to match(/
+      Exception:\sRuntimeError:\souter
+      .+
+      Caused\sby:\sRuntimeError:\sinner
+    /xm)
+  end
+end
