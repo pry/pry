ff6263fd9ddf2d689ecfa8fa7eb1216bfad441a8
Akira Matsuda <ronnie@dio.jp>
2016-12-08 03:51:38 +0900

    Avoid calling Ruby 2.4+ String#pretty_print in ColorPrinter
    Ruby 2.4+ defines String's own pretty_print that prints multiline Strings prettier
    see: https://bugs.ruby-lang.org/issues/12664
    
    fixes #1585

--- lib/pry/color_printer.rb
+++ lib/pry/color_printer.rb
@@ -31,7 +31,13 @@ class Pry
     end
 
     def pp(obj)
-      super
+      if String === obj
+        # Avoid calling Ruby 2.4+ String#pretty_print that prints multiline
+        # Strings prettier
+        Object.instance_method(:pretty_print).bind(obj).call
+      else
+        super
+      end
     rescue => e
       raise if e.is_a? Pry::Pager::StopPaging
       begin
