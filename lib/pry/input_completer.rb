8c4083791a41d82c5f04b599a725c05d7a14c8f7
Dmitry Gutov <dgutov@yandex.ru>
2016-12-12 03:46:57 +0200

    Fix a typo

--- lib/pry/input_completer.rb
+++ lib/pry/input_completer.rb
@@ -252,7 +252,7 @@ class Pry::InputCompleter
     end
 
     # FIXME: Add Pry here as well?
-    %w(IRB SLEX RubyLex RubyToken).each do |module_name|
+    %w(IRB SLex RubyLex RubyToken).each do |module_name|
       sym = module_name.to_sym
       next unless Object.const_defined?(sym)
       scanner.call(Object.const_get(sym))
