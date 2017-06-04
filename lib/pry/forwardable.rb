819d4c1bb6815a0e56c0ae1cc4a492aeb6e656b4
robert <robert@jazzonmymind.xyz>
2017-06-04 22:28:25 +0100

    fix warning about splat

--- lib/pry/forwardable.rb
+++ lib/pry/forwardable.rb
@@ -17,7 +17,7 @@ class Pry
           instance_variable_get(target).__send__(private_delegate, *a, &b)
         end
       end
-      class_eval { private *private_delegates }
+      class_eval { private(*private_delegates) }
     end
   end
 end
