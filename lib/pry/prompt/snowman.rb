f5e71b3fc4dfd9c79a07ecc3994d752167e0473f
0xAB <0xAB@protonmail.com>
2017-09-01 15:29:11 +0100

    use lvar instead of extend

--- lib/pry/prompt/snowman.rb
+++ lib/pry/prompt/snowman.rb
@@ -13,24 +13,24 @@ module Pry::Prompt::Snowman
   COFFEE = "☕"
 
   def prompt(obj, nest_level, pry, wait)
-    extend pry.helpers if not pry.helpers === self
+    h = pry.helpers
     e = wait ? "*" : ">"
     input_size = pry.input_array.size
     [
-      bright_yellow(" #{input_size} "),
-      " #{ruby_info(pry)} ",
-      "#{PICK} (#{bold(Pry.view_clip(obj))})#{':%s' % nest_level if not nest_level.zero?}#{e}",
+      h.bright_yellow(" #{input_size} "),
+      " #{ruby_info(pry, h)} ",
+      "#{PICK} (#{h.bold(Pry.view_clip(obj))})#{':%s' % nest_level if not nest_level.zero?}#{e}",
     ].compact.join
   end
 
-  def ruby_info(pry)
+  def ruby_info(pry, h)
     case
-    when jruby?
+    when h.jruby?
       str = " #{COFFEE}  #{RUBY_ENGINE}-#{RUBY_VERSION} "
-      bright_white_on_red(str)
+      h.bright_white_on_red(str)
     else
       str = " #{GEM_STONE}  #{RUBY_ENGINE}-#{RUBY_VERSION} "
-      bright_white_on_red(str)
+      h.bright_white_on_red(str)
     end
   end
   private :ruby_info
