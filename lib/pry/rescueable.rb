f298fb99e3d78ea6cabf6d07e42946c76f63ce60
robert <r@newton.osaka>
2017-09-07 12:11:31 +0100

    add docs [ci skip]

--- lib/pry/rescueable.rb
+++ lib/pry/rescueable.rb
@@ -1,3 +1,23 @@
+#
+# Pry.Rescueable() is a method that returns an object for use with
+# the 'rescue' keyword. The object decides if an exception should be
+# rescued, or raised. See {Pry.config.exception_whitelist} for a default
+# list of exceptions not rescued via this method.
+#
+# @param [Pry] pry
+#   An instance of `Pry`, where `config.exception_whitelist` is read from.
+#
+# @return [Module]
+#
+# @example
+#
+#   # In some Pry command...
+#   begin
+#     _pry_.pager.page(str)
+#   rescue Pry.Rescuable(_pry_) => e
+#     puts [e.class, e.message]
+#   end
+#
 def Pry.Rescuable(pry = nil)
   Module.new do
     define_singleton_method(:===) do |e|
