78ea7e5eb2a0c215f4159d8c5d1d2eaad531a1e1
r-obert <r@newton.osaka>
2017-11-11 00:30:13 +0100

    implement a more concise solution offered by @rf-

--- lib/pry/config/behavior.rb
+++ lib/pry/config/behavior.rb
@@ -188,8 +188,7 @@ module Pry::Config::Behavior
   end
 
   def respond_to_missing?(key, include_all=false)
-    key = key.to_s
-    key = key[0..-2] if key.end_with? ASSIGNMENT
+    key = key.to_s.chomp(ASSIGNMENT)
     key?(key) or @default.respond_to?(key) or super(key, include_all)
   end
 
