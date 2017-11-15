3f4527c283ffbd408695de3bdbc89582f96ca666
r-obert <r@newton.osaka>
2017-11-15 23:20:49 +0100

    don't assume filesystem uses /

--- lib/pry/commands.rb
+++ lib/pry/commands.rb
@@ -3,5 +3,5 @@ Pry::Commands = Pry::CommandSet.new
 
 # Use `Kernel.require` to avoid the rubygems monkey patch.
 # Also use an absolute path to avoid scanning $LOAD_PATH.
-glob = File.expand_path File.join(__FILE__, "../commands/*.rb")
+glob = File.expand_path File.join(__FILE__, "..", "commands", "*.rb")
 Dir[glob].each{|f| Kernel.require(f) }
