a41359730f67c0a134012748faf361232a117e00
r-obert <r@newton.osaka>
2017-11-27 23:30:45 +0100

    fallback to 'Pry' if _pry_ is not in scope (backwards compatible)

--- lib/pry/helpers/text.rb
+++ lib/pry/helpers/text.rb
@@ -14,21 +14,21 @@ module Pry::Helpers::Text
     }
 
   COLORS.each_pair do |color, value|
-    define_method color do |text, pry=(defined?(_pry_) and _pry_)|
+    define_method color do |text, pry=((defined?(_pry_) and _pry_) or Pry)|
       (pry and pry.color) ? "\033[0;#{30+value}m#{text}\033[0m" : text
     end
 
-    define_method "bright_#{color}" do |text, pry=(defined?(_pry_) and _pry_)|
-      (pry and pry.color) ? "\033[1;#{30+value}m#{text}\033[0m" : text
+    define_method "bright_#{color}" do |text, pry=((defined?(_pry_) and _pry_) or Pry)|
+      (pry.color) ? "\033[1;#{30+value}m#{text}\033[0m" : text
     end
 
     COLORS.each_pair do |bg_color, bg_value|
-      define_method "#{color}_on_#{bg_color}" do |text, pry=(defined?(_pry_) and _pry_)|
-        (pry and pry.color) ? "\033[0;#{30 + value};#{40 + bg_value}m#{text}\033[0m" : text
+      define_method "#{color}_on_#{bg_color}" do |text, pry=((defined?(_pry_) and _pry_) or Pry)|
+        (pry.color) ? "\033[0;#{30 + value};#{40 + bg_value}m#{text}\033[0m" : text
       end
 
-      define_method "bright_#{color}_on_#{bg_color}" do |text, pry=(defined?(_pry_) and _pry_)|
-        (pry and pry.color) ? "\033[1;#{30 + value};#{40 + bg_value}m#{text}\033[0m" : text
+      define_method "bright_#{color}_on_#{bg_color}" do |text, pry=((defined?(_pry_) and _pry_) or Pry)|
+        (pry.color) ? "\033[1;#{30 + value};#{40 + bg_value}m#{text}\033[0m" : text
       end
     end
   end
@@ -37,7 +37,7 @@ module Pry::Helpers::Text
   #
   # @param  [String, #to_s] text
   # @return [String] _text_ stripped of any color codes.
-  def strip_color(text, pry=(defined?(_pry_) and _pry_))
+  def strip_color(text, pry=((defined?(_pry_) and _pry_) or Pry))
     text.to_s.gsub(/(\001)?\e\[.*?(\d)+m(\002)?/ , '')
   end
 
@@ -45,8 +45,60 @@ module Pry::Helpers::Text
   #
   # @param [String, #to_s] text
   # @return [String] _text_
-  def bold(text, pry=(defined?(_pry_) and _pry_))
-    (pry and pry.color) ? "\e[1m#{text}\e[0m" : text
+  def bold text, pry=((defined?(_pry_) and _pry_) or Pry)
+    (pry.color) ? "\e[1m#{text}\e[0m" : text
+  end
+
+  #
+  # @yield
+  #   Yields a block with color turned off.
+  #
+  # @return [void]
+  #
+  def no_color pry=((defined?(_pry_) and _pry_) or Pry)
+    boolean = pry.config.color
+    pry.config.color = false
+    yield
+  ensure
+    pry.config.color = boolean
+  end
+
+  #
+  # @yield
+  #   Yields a block with paging turned off.
+  #
+  # @return [void]
+  #
+  def no_pager pry=((defined?(_pry_) and _pry_) or Pry)
+    boolean = pry.config.pager
+    pry.config.pager = false
+    yield
+  ensure
+    pry.config.pager = boolean
+  end
+
+  # Returns _text_ in a numbered list, beginning at _offset_.
+  #
+  # @param  [#each_line] text
+  # @param  [Fixnum] offset
+  # @return [String]
+  def with_line_numbers text, offset, color=:blue, pry=((defined?(_pry_) and _pry_) or Pry)
+    lines = text.each_line.to_a
+    max_width = (offset + lines.count).to_s.length
+    lines.each_with_index.map do |line, index|
+      adjusted_index = (index + offset).to_s.rjust(max_width)
+      pry.color ?
+      "#{self.send(color, adjusted_index)}: #{line}" :
+      "#{adjusted_index}: #{line}"
+    end.join
+  end
+
+  # Returns _text_ indented by _chars_ spaces.
+  #
+  # @param [String] text
+  # @param [Fixnum] chars
+  def indent(text, chars)
+    text.lines.map { |l| "#{' ' * chars}#{l}" }.join
   end
 
   # Returns `text` in the default foreground colour.
@@ -63,53 +115,4 @@ module Pry::Helpers::Text
   end
   alias_method :bright_default, :bold
 
-  #
-  # @yield
-  #   Yields a block with color turned off.
-  #
-  # @return [void]
-  #
-  def no_color pry=(defined?(_pry_) and _pry_)
-    boolean = pry.config.color
-    pry.config.color = false
-    yield
-  ensure
-    pry.config.color = boolean
-  end
-
-  #
-  # @yield
-  #   Yields a block with paging turned off.
-  #
-  # @return [void]
-  #
-  def no_pager pry=(defined?(_pry_) and _pry_)
-    boolean = pry.config.pager
-    pry.config.pager = false
-    yield
-  ensure
-    pry.config.pager = boolean
-  end
-
-  # Returns _text_ in a numbered list, beginning at _offset_.
-  #
-  # @param  [#each_line] text
-  # @param  [Fixnum] offset
-  # @return [String]
-  def with_line_numbers(text, offset, color=:blue)
-    lines = text.each_line.to_a
-    max_width = (offset + lines.count).to_s.length
-    lines.each_with_index.map do |line, index|
-      adjusted_index = (index + offset).to_s.rjust(max_width)
-      "#{self.send(color, adjusted_index)}: #{line}"
-    end.join
-  end
-
-  # Returns _text_ indented by _chars_ spaces.
-  #
-  # @param [String] text
-  # @param [Fixnum] chars
-  def indent(text, chars)
-    text.lines.map { |l| "#{' ' * chars}#{l}" }.join
-  end
 end
