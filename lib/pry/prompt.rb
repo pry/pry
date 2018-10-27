ae531ac738675cc8294d59c4f58e8043946afe11
Kyrylo Silin <silin@kyrylo.org>
2018-10-28 05:04:13 +0800

    prompt: use explicit nesting for class definition
    Per https://github.com/rubocop-hq/ruby-style-guide#namespace-definition

--- lib/pry/prompt.rb
+++ lib/pry/prompt.rb
@@ -1,26 +1,28 @@
-class Pry::Prompt
-  MAP = {
-    "default" => {
-      value: Pry::DEFAULT_PROMPT,
-      description: "The default Pry prompt. Includes information about the\n" \
-                   "current expression number, evaluation context, and nesting\n" \
-                   "level, plus a reminder that you're using Pry."
-    },
+class Pry
+  class Prompt
+    MAP = {
+      "default" => {
+        value: Pry::DEFAULT_PROMPT,
+        description: "The default Pry prompt. Includes information about the\n" \
+                     "current expression number, evaluation context, and nesting\n" \
+                     "level, plus a reminder that you're using Pry."
+      },
 
-    "simple" => {
-      value: Pry::SIMPLE_PROMPT,
-      description: "A simple '>>'."
-    },
+      "simple" => {
+        value: Pry::SIMPLE_PROMPT,
+        description: "A simple '>>'."
+      },
 
-    "nav" => {
-      value: Pry::NAV_PROMPT,
-      description: "A prompt that displays the binding stack as a path and\n" \
-                   "includes information about _in_ and _out_."
-    },
+      "nav" => {
+        value: Pry::NAV_PROMPT,
+        description: "A prompt that displays the binding stack as a path and\n" \
+                     "includes information about _in_ and _out_."
+      },
 
-    "none" => {
-      value: Pry::NO_PROMPT,
-      description: "Wave goodbye to the Pry prompt."
+      "none" => {
+        value: Pry::NO_PROMPT,
+        description: "Wave goodbye to the Pry prompt."
+      }
     }
- }
+  end
 end
