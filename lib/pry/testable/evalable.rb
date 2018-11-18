289397432cd2610055ecdb86fe5785d0a52bd815
robert <robert@jazzonmymind.xyz>
2018-11-18 20:28:08 +0100

    add pry_repl_tester to Evalable

--- lib/pry/testable/evalable.rb
+++ lib/pry/testable/evalable.rb
@@ -8,6 +8,21 @@ module Pry::Testable::Evalable
     end
   end
 
+  #
+  # For super-high-level integration testing.
+  #
+  # @see Pry::Testable::ReplTester
+  #
+  # @example
+  #   pry_repl_tester do |repl|
+  #     repl.enter_input '_pry_.config.prompt_name = "foobar"'
+  #     expect(repl.last_prompt).to match('foobar')
+  #   end
+  #
+  def pry_repl_tester(&b)
+    Pry::Testable::ReplTester.start(&b)
+  end
+
   def pry_eval(*eval_strs)
     b = String === eval_strs.first ? Pry.toplevel_binding : Pry.binding_for(eval_strs.shift)
     pry_tester(b).eval(*eval_strs)
