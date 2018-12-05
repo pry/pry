e2e685008d993e953ce13567d699e63ba172c1c8
robert <robert@jazzonmymind.xyz>
2018-12-05 05:56:07 +0100

    Add spec examples

--- spec/integration/cli_spec.rb
+++ spec/integration/cli_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe 'The bin/pry CLI' do
   context "ARGV forwarding" do
     let(:code) { "p(ARGV) and exit".shellescape }
 
+    it "forwards ARGV as an empty array when - is passed without following arguments" do
+      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} -`.chomp
+      expect(out).to eq([].inspect)
+    end
+
+    it "forwards ARGV as an empty array when -- is passed without following arguments" do
+      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} --`.chomp 
+      expect(out).to eq([].inspect)
+    end
+
     it "forwards its remaining arguments as ARGV when - is passed" do
       out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} - 1 -foo --bar --baz daz`.chomp
       expect(out).to eq(%w[1 -foo --bar --baz daz].inspect)
