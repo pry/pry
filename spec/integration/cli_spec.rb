534ee43acd9f81e1f1d22e288eb427463cc0d099
robert <robert@jazzonmymind.xyz>
2018-12-05 05:44:14 +0100

    Update spec

--- spec/integration/cli_spec.rb
+++ spec/integration/cli_spec.rb
@@ -3,15 +3,16 @@ RSpec.describe 'The bin/pry CLI' do
   let(:pry_dir) { File.expand_path(File.join(__FILE__, '../../../lib')).shellescape }
 
   context "ARGV forwarding" do
-    let(:code) { 'p(ARGV) and exit'.shellescape }
+    let(:code) { "p(ARGV) and exit".shellescape }
+
     it "forwards its remaining arguments as ARGV when - is passed" do
-      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} - -a --b 3`.chomp
-      expect(out).to eq(%w[-a --b 3].inspect)
+      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} - 1 -foo --bar --baz daz`.chomp
+      expect(out).to eq(%w[1 -foo --bar --baz daz].inspect)
     end
 
     it "forwards its remaining arguments as ARGV when -- is passed" do
-      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} -- -a --b 3`.chomp
-      expect(out).to eq(%w[-a --b 3].inspect)
+      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} -- 1 -foo --bar --baz daz`.chomp
+      expect(out).to eq(%w[1 -foo --bar --baz daz].inspect)
     end
   end
 end
