2973074e0b14b6183dfb1f983f531b2906fe7e78
robert <robert@jazzonmymind.xyz>
2018-12-13 15:39:22 +0100

    Update spec

--- spec/integration/cli_spec.rb
+++ spec/integration/cli_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe 'The bin/pry CLI' do
 
     it 'adds multiple additional paths to $LOAD_PATH' do
       code = 'p($LOAD_PATH) and exit'
-      out = `#{ruby} -I#{pry_dir} bin/pry -I /added/at/cli -I /added/at/cli/also -e '#{code}'`
-      expect(out).to include('/added/at/cli')
+      out = `#{ruby} -I#{pry_dir} bin/pry -I /added-1/at/cli -I /added/at/cli/also -e '#{code}'`
+      expect(out).to include('/added-1/at/cli')
       expect(out).to include('/added/at/cli/also')
     end
   end
