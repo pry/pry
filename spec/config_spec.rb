70aa7b9b69a05e80f835d343347b9493719077ad
robert <robert@jazzonmymind.xyz>
2018-12-13 07:56:54 +0100

    Update spec

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -11,11 +11,18 @@ RSpec.describe Pry::Config do
       expect(local.default).to eq(local)
     end
 
-    it "recursively creates Pry::Config objects from a Hash" do
+    it "recursively walks a Hash" do
       h = {'foo1' => {'foo2' => {'foo3' => 'foobar'}}}
       default = described_class.from_hash(h)
       expect(default.foo1).to be_instance_of(described_class)
       expect(default.foo1.foo2).to be_instance_of(described_class)
+    end
+
+    it "recursively walks an Array" do
+      c = described_class.from_hash(ary: [{number: 2}, Object, BasicObject.new])
+      expect(c.ary[0].number).to eq(2)
+      expect(c.ary[1]).to eq(Object)
+      expect(BasicObject === c.ary[2]).to be(true)
     end
   end
 
@@ -32,17 +39,6 @@ RSpec.describe Pry::Config do
       local = described_class.from_hash(output: 'foobar')
       local.extend(Module.new { def output(); 'broken'; end })
       expect(local.output).to eq('foobar')
-    end
-  end
-
-  describe "from_hash" do
-    it "recursively walks its argument and transforms Hash objects into Pry::Config objects" do
-      c = described_class.from_hash(number: 1, ary: [{number: 2}, Object, BasicObject.new], c: {str: 'hello'})
-      expect(c.number).to eq(1)
-      expect(c.ary[0].number).to eq(2)
-      expect(c.ary[1]).to eq(Object)
-      expect(BasicObject === c.ary[2]).to be(true)
-      expect(c.c.str).to eq('hello')
     end
   end
 
