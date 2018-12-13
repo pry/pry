9df9f9e59413327a18f92fc8f6bf8405f92fb003
r-obert <r-obert@users.noreply.github.com>
2018-12-13 12:08:57 +0100

    Fix the 'clear-screen' command (#1908)
    * Fix the 'clear-screen' command
    
        [1] pry(main)> clear-screen
        NameError: uninitialized constant Pry::Command::ClearScreen::Helpers
        Did you mean?  Pry::Helpers
    
        [1] pry(main)> pry-version
        Pry version: 0.12.2 on Ruby 2.5.1.

---
+++ spec/commands/clear_screen_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe "clear-screen" do
+  before do
+    @t = pry_tester
+  end
+
+  it 'calls the "clear" command on non-Windows platforms' do
+    expect(Pry.config.system).to receive(:call).with(an_instance_of(Pry::Output), 'clear', an_instance_of(Pry))
+    @t.process_command 'clear-screen'
+  end
+
+  it 'calls the "cls" command on Windows' do
+    expect(Pry::Helpers::Platform).to receive(:windows?).at_least(:once).and_return(true)
+    expect(Pry.config.system).to receive(:call).with(an_instance_of(Pry::Output), 'cls', an_instance_of(Pry))
+    @t.process_command 'clear-screen'
+  end
+end
