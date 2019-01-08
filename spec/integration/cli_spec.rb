089b2e8fb05f1b24c70a6647788e0dd12d2347a3
robert <robert@jazzonmymind.xyz>
2019-01-09 00:19:11 +0100

    Move fix to the test case

--- spec/integration/cli_spec.rb
+++ spec/integration/cli_spec.rb
@@ -1,27 +1,32 @@
 RSpec.describe 'The bin/pry CLI' do
   let(:ruby) { RbConfig.ruby.shellescape }
   let(:pry_dir) { File.expand_path(File.join(__FILE__, '../../../lib')).shellescape }
+  # Pry will emit silent garbage because of our auto indent feature.
+  # This lambda cleans the output of that garbage.
+  clean_output = lambda do |out|
+    out.strip.sub("\e[0G", "")
+  end
 
   context "ARGV forwarding" do
     let(:code) { "p(ARGV) and exit".shellescape }
 
     it "forwards ARGV as an empty array when - is passed without following arguments" do
-      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} -`.chomp
+      out = clean_output.call(`#{ruby} -I#{pry_dir} bin/pry -e #{code} -`)
       expect(out).to eq([].inspect)
     end
 
     it "forwards ARGV as an empty array when -- is passed without following arguments" do
-      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} --`.chomp
+      out = clean_output.call(`#{ruby} -I#{pry_dir} bin/pry -e #{code} --`)
       expect(out).to eq([].inspect)
     end
 
     it "forwards its remaining arguments as ARGV when - is passed" do
-      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} - 1 -foo --bar --baz daz`.chomp
+      out = clean_output.call(`#{ruby} -I#{pry_dir} bin/pry -e #{code} - 1 -foo --bar --baz daz`)
       expect(out).to eq(%w[1 -foo --bar --baz daz].inspect)
     end
 
     it "forwards its remaining arguments as ARGV when -- is passed" do
-      out = `#{ruby} -I#{pry_dir} bin/pry --no-correct-indent -e #{code} -- 1 -foo --bar --baz daz`.chomp
+      out = clean_output.call(`#{ruby} -I#{pry_dir} bin/pry -e #{code} -- 1 -foo --bar --baz daz`)
       expect(out).to eq(%w[1 -foo --bar --baz daz].inspect)
     end
   end
@@ -29,13 +34,13 @@ RSpec.describe 'The bin/pry CLI' do
   context '-I path' do
     it 'adds an additional path to $LOAD_PATH' do
       code = 'p($LOAD_PATH) and exit'
-      out = `#{ruby} -I#{pry_dir} bin/pry -I /added/at/cli -e '#{code}'`
+      out = clean_output.call(`#{ruby} -I#{pry_dir} bin/pry -I /added/at/cli -e '#{code}'`)
       expect(out).to include('/added/at/cli')
     end
 
     it 'adds multiple additional paths to $LOAD_PATH' do
       code = 'p($LOAD_PATH) and exit'
-      out = `#{ruby} -I#{pry_dir} bin/pry -I /added-1/at/cli -I /added/at/cli/also -e '#{code}'`
+      out = clean_output.call(`#{ruby} -I#{pry_dir} bin/pry -I /added-1/at/cli -I /added/at/cli/also -e '#{code}'`)
       expect(out).to include('/added-1/at/cli')
       expect(out).to include('/added/at/cli/also')
     end
