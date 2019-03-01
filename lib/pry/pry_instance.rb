a892d00506cf7dfaac4d69752363e1f9d73a0d2a
Kyrylo Silin <silin@kyrylo.org>
2019-03-02 01:50:44 +0200

    rubocop: fix offences of the Style/Encoding cop

--- lib/pry/pry_instance.rb
+++ lib/pry/pry_instance.rb
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 ##
 # Pry is a powerful alternative to the standard IRB shell for Ruby. It
 # features syntax highlighting, a flexible plugin architecture, runtime
--- lib/pry/terminal.rb
+++ lib/pry/terminal.rb
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 class Pry
   class Terminal
     class << self
