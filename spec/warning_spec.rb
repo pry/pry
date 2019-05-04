224b575fc7f19b89c64b06b69054bfbbe2f7c800
Kyrylo Silin <silin@kyrylo.org>
2019-05-04 20:18:45 +0300

    warning: print file and line of the calling frame
    We want to print the calling frame, so we can point out who exactly triggered
    it. The current behaviour is less useful (we print the line that called the
    warning).

--- spec/warning_spec.rb
+++ spec/warning_spec.rb
@@ -1,9 +1,7 @@
 RSpec.describe Pry::Warning do
   describe "#warn" do
-    it "prints a warning with file and line" do
-      expect(Kernel).to receive(:warn).with(
-        "#{__FILE__}:#{__LINE__ + 2}: warning: foo bar"
-      )
+    it "prints message with file and line of the calling frame" do
+      expect(Kernel).to receive(:warn).with(/.+\.rb:\d+: warning: foo bar/)
       described_class.warn('foo bar')
     end
   end
