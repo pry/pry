pry/pry

commit 78caffbcbca9ed96de1ee695857872d2e7dd57d4

Dedup .pryrc across symlinks too

lib/pry/pry_class.rb
@@ -68,26 +68,33 @@ class Pry
 
   # Load the given file in the context of `Pry.toplevel_binding`
   # @param [String] file_name The unexpanded file path.
-  def self.load_file_at_toplevel(file_name)
-    full_name = File.expand_path(file_name)
-    begin
-      toplevel_binding.eval(File.read(full_name), full_name) if File.exists?(full_name)
-    rescue RescuableException => e
-      puts "Error loading #{file_name}: #{e}\n#{e.backtrace.first}"
-    end
+  def self.load_file_at_toplevel(file)
+    toplevel_binding.eval(File.read(file), file)
+  rescue RescuableException => e
+    puts "Error loading #{file}: #{e}\n#{e.backtrace.first}"
   end
 
-  # Load the rc files given in the `Pry::RC_FILES` array.
+  # Load HOME_RC_FILE and LOCAL_RC_FILE if appropriate
   # This method can also be used to reload the files if they have changed.
-  def self.load_rc
-    load_file_at_toplevel(HOME_RC_FILE)
+  def self.load_rc_files
+    rc_files_to_load.each do |file|
+      load_file_at_toplevel(file)
+    end
   end
 
   # Load the local RC file (./.pryrc)
-  def self.load_local_rc
-    unless File.expand_path(HOME_RC_FILE) == File.expand_path(LOCAL_RC_FILE)
-      load_file_at_toplevel(LOCAL_RC_FILE)
-    end
+  def self.rc_files_to_load
+    files = []
+    files << HOME_RC_FILE if Pry.config.should_load_rc
+    files << LOCAL_RC_FILE if Pry.config.should_load_local_rc
+    files.map { |file| real_path_to(file) }.compact.uniq
+  end
+
+  # Expand a file to its canonical name (following symlinks as appropriate)
+  def self.real_path_to(file)
+    Pathname.new(File.expand_path(file)).realpath.to_s
+  rescue Errno::ENOENT
+    nil
   end
 
   # Load any Ruby files specified with the -r flag on the command line.
@@ -112,8 +119,7 @@ class Pry
 
     # note these have to be loaded here rather than in pry_instance as
     # we only want them loaded once per entire Pry lifetime.
-    load_rc if Pry.config.should_load_rc
-    load_local_rc if Pry.config.should_load_local_rc
+    load_rc_files
     load_plugins if Pry.config.should_load_plugins
     load_requires if Pry.config.should_load_requires
     load_history if Pry.config.history.should_load

spec/pryrc_spec.rb
@@ -1,0 +1,94 @@
+require 'helper'
+
+describe Pry do
+  describe 'loading rc files' do
+    before do
+      Pry::HOME_RC_FILE.replace "spec/fixtures/testrc"
+      Pry::LOCAL_RC_FILE.replace "spec/fixtures/testrc/../testrc"
+      Pry.instance_variable_set(:@initial_session, true)
+      Pry.config.should_load_rc = true
+      Pry.config.should_load_local_rc = true
+    end
+
+    after do
+      Pry::HOME_RC_FILE.replace "~/.pryrc"
+      Pry::LOCAL_RC_FILE.replace "./.pryrc"
+      Pry.config.should_load_rc = false
+      Object.remove_const(:TEST_RC) if defined?(TEST_RC)
+    end
+
+    it "should never run the rc file twice" do
+      Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new)
+      TEST_RC.should == [0]
+
+      Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new)
+      TEST_RC.should == [0]
+    end
+
+    it "should not load the rc file twice if it's symlinked differently" do
+      Pry::HOME_RC_FILE.replace "spec/fixtures/testrc"
+      Pry::LOCAL_RC_FILE.replace "spec/fixtures/testlinkrc"
+
+      Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new)
+
+      TEST_RC.should == [0]
+    end
+
+    it "should not load the pryrc if it cannot expand ENV[HOME]" do
+      old_home = ENV['HOME']
+      ENV['HOME'] = nil
+      Pry.config.should_load_rc = true
+      lambda { Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new) }.should.not.raise
+
+      ENV['HOME'] = old_home
+    end
+
+    it "should not run the rc file at all if Pry.config.should_load_rc is false" do
+      Pry.config.should_load_rc = false
+      Pry.config.should_load_local_rc = false
+      Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new)
+      Object.const_defined?(:TEST_RC).should == false
+    end
+
+    describe "that raise exceptions" do
+      before do
+        Pry::HOME_RC_FILE = "spec/fixtures/testrcbad"
+        Pry.config.should_load_local_rc = false
+
+        putsed = nil
+
+        # YUCK! horrible hack to get round the fact that output is not configured
+        # at the point this message is printed.
+        (class << Pry; self; end).send(:define_method, :puts) { |str|
+          putsed = str
+        }
+
+        @doing_it = lambda{
+          Pry.start(self, :input => StringIO.new("Object::TEST_AFTER_RAISE=1\nexit-all\n"), :output => StringIO.new)
+          putsed
+        }
+      end
+
+      after do
+        Object.remove_const(:TEST_BEFORE_RAISE)
+        Object.remove_const(:TEST_AFTER_RAISE)
+        (class << Pry; undef_method :puts; end)
+      end
+
+      it "should not raise exceptions" do
+        @doing_it.should.not.raise
+      end
+
+      it "should continue to run pry" do
+        @doing_it[]
+        Object.const_defined?(:TEST_BEFORE_RAISE).should == true
+        Object.const_defined?(:TEST_AFTER_RAISE).should == true
+      end
+
+      it "should output an error" do
+        @doing_it.call.split("\n").first.should =~
+          %r{Error loading .*spec/fixtures/testrcbad: messin with ya}
+      end
+    end
+  end
+end

lib/pry.rb
@@ -212,6 +212,7 @@ require 'coderay'
 require 'slop'
 require 'rbconfig'
 require 'tempfile'
+require 'pathname'
 
 begin
   require 'readline'

spec/fixtures/testlinkrc
@@ -1,0 +1,1 @@
+testrc

spec/pry_spec.rb
@@ -281,90 +281,6 @@ describe Pry do
         end
       end
 
-      describe "test loading rc files" do
-        before do
-          Pry::HOME_RC_FILE.replace "spec/fixtures/testrc"
-          Pry::LOCAL_RC_FILE.replace "spec/fixtures/testrc/../testrc"
-          Pry.instance_variable_set(:@initial_session, true)
-        end
-
-        after do
-          Pry::HOME_RC_FILE.replace "~/.pryrc"
-          Pry::LOCAL_RC_FILE.replace "./.pryrc"
-          Pry.config.should_load_rc = false
-          Object.remove_const(:TEST_RC) if defined?(TEST_RC)
-        end
-
-        it "should never run the rc file twice" do
-          Pry.config.should_load_rc = true
-
-          Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new)
-          TEST_RC.should == [0]
-
-          Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new)
-          TEST_RC.should == [0]
-        end
-
-        it "should not load the pryrc if it cannot expand ENV[HOME]" do
-          old_home = ENV['HOME']
-          old_rc = Pry.config.should_load_rc
-          ENV['HOME'] = nil
-          Pry.config.should_load_rc = true
-          lambda { Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new) }.should.not.raise
-
-          ENV['HOME'] = old_home
-          Pry.config.should_load_rc = old_rc
-        end
-
-        it "should not run the rc file at all if Pry.config.should_load_rc is false" do
-          Pry.config.should_load_rc = false
-          Pry.start(self, :input => StringIO.new("exit-all\n"), :output => StringIO.new)
-          Object.const_defined?(:TEST_RC).should == false
-        end
-
-        describe "that raise exceptions" do
-          before do
-            Pry::HOME_RC_FILE = "spec/fixtures/testrcbad"
-            Pry.config.should_load_rc = true
-            Pry.config.should_load_local_rc = false
-
-            putsed = nil
-
-            # YUCK! horrible hack to get round the fact that output is not configured
-            # at the point this message is printed.
-            (class << Pry; self; end).send(:define_method, :puts) { |str|
-              putsed = str
-            }
-
-            @doing_it = lambda{
-              Pry.start(self, :input => StringIO.new("Object::TEST_AFTER_RAISE=1\nexit-all\n"), :output => StringIO.new)
-              putsed
-            }
-          end
-
-          after do
-            Object.remove_const(:TEST_BEFORE_RAISE)
-            Object.remove_const(:TEST_AFTER_RAISE)
-            (class << Pry; undef_method :puts; end)
-          end
-
-          it "should not raise exceptions" do
-            @doing_it.should.not.raise
-          end
-
-          it "should continue to run pry" do
-            @doing_it[]
-            Object.const_defined?(:TEST_BEFORE_RAISE).should == true
-            Object.const_defined?(:TEST_AFTER_RAISE).should == true
-          end
-
-          it "should output an error" do
-            @doing_it.call.split("\n").first.should ==
-              "Error loading spec/fixtures/testrcbad: messin with ya"
-          end
-        end
-      end
-
       describe "nesting" do
         after do
           Pry.reset_defaults

commit 621ff63d0f1087874b05ac2e876944252ed5008d

Honor Pry.config.should_load_local_rc = false in global RC file
The `Pry.config.should_load_local_rc` flag was first added for #612. The
intended use case was to set it to `false` in your "global" RC file
(~/.pryrc, ~/.config/pry/pryrc, etc) to disable local RC files.

However, the refactoring in 78caffbcbca9ed96de1ee695857872d2e7dd57d4
broke this functionality, because `Pry.rc_files_to_load` evaluates
`Pry.config.should_load_local_rc` before we ever load anything via
`Pry.load_rc_files`. The only way `should_load_local_rc` gets disabled
before this point is if we pass in the `-f` flag to Pry, which disables
*all* RC files.

The main point of `Pry.rc_files_to_load` is that it resolves the RC
paths and de-duplicates them, in case the global one symlinks to the
local one or vice versa. But it attempts to do this "ahead of time",
generating the list of files first before we ever load any of them, then
calling `Array#uniq`. This dedups the paths, but populating the array in
the first place means we check the `should_load_local_rc` boolean before
the global RC ever has a chance to set it.

This PR undoes some of that refactoring, bringing it closer to the old
code that had separate steps for `Pry.load_rc` and `Pry.load_local_rc`,
so we can check the config flag after the side effect of loading the
global RC file. However, it maintains the more general interface of
`Pry.load_rc_files`, in case other types of RC files are added in the
future. No matter the type of RC file, they all funnel through
`Pry.load_rc_file`, which now handles resolving paths and preventing
duplicates. It does this by maintaining `Pry.rc_files_loaded`, shifting
the logic from the more static "what will we load in the future?" to the
more dynamic "what have we loaded in the past?"

The `Pry.rc_files_to_load` method is removed by this PR, since it's not
necessarily an accurate reflection of the files that ultimately do get
loaded. Because it was public, this is technically a breaking change to

lib/pry/pry_class.rb
@@ -75,19 +75,27 @@ class Pry
   # Load RC files if appropriate This method can also be used to reload the
   # files if they have changed.
   def self.load_rc_files
-    rc_files_to_load.each do |file|
-      critical_section do
-        load_file_at_toplevel(file)
-      end
-    end
+    rc_files_loaded.clear
+    load_rc_file(Pry.config.rc_file) if Pry.config.rc_file && Pry.config.should_load_rc
+    load_rc_file(LOCAL_RC_FILE) if Pry.config.should_load_local_rc
   end
 
-  # Load the local RC file (./.pryrc)
-  def self.rc_files_to_load
-    files = []
-    files << Pry.config.rc_file if Pry.config.rc_file && Pry.config.should_load_rc
-    files << LOCAL_RC_FILE if Pry.config.should_load_local_rc
-    files.map { |file| real_path_to(file) }.compact.uniq
+  def self.load_rc_file(file)
+    file = real_path_to(file)
+    return if file.nil?
+    return if rc_files_loaded.include?(file)
+
+    critical_section do
+      load_file_at_toplevel(file)
+    end
+
+    rc_files_loaded << file
+  end
+  private_class_method :load_rc_file
+
+  # @return [Array<String>] the files that have been loaded by {.load_rc_files}
+  def self.rc_files_loaded
+    @rc_files_loaded ||= []
   end
 
   # Expand a file to its canonical name (following symlinks as appropriate)

spec/fixtures/globalrc
@@ -1,0 +1,2 @@
+LOADED_LOCAL_RC = false
+Pry.config.should_load_local_rc = false

spec/fixtures/localrc
@@ -1,0 +1,1 @@
+LOADED_LOCAL_RC = true

spec/pryrc_spec.rb
@@ -66,6 +66,16 @@ describe Pry do
       expect(Object.const_defined?(:TEST_RC)).to eq false
     end
 
+    it "should not load the local rc if the global rc disables it" do
+      Pry.config.rc_file = 'spec/fixtures/globalrc'
+      stub_const('Pry::LOCAL_RC_FILE', 'spec/fixtures/localrc')
+      Pry.config.should_load_rc = true
+      Pry.config.should_load_local_rc = true
+      Pry.start(self, input: StringIO.new("exit-all\n"), output: StringIO.new)
+      expect(LOADED_LOCAL_RC).to be false
+      Object.remove_const(:LOADED_LOCAL_RC)
+    end
+
     describe "that raise exceptions" do
       before do
         Pry.config.rc_file = 'spec/fixtures/testrcbad'