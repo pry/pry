d9211083b1c0ace1910660a7159533e9002c2bd1
hyuraku <32809703+hyuraku@users.noreply.github.com>
2023-08-30 22:30:47 +0900

    repair standard error spec

--- spec/exception_handler_spec.rb
+++ spec/exception_handler_spec.rb
@@ -25,9 +25,14 @@ RSpec.describe Pry::ExceptionHandler do
 
       it "prints standard error message" do
         described_class.handle_exception(output, exception, pry_instance)
-        expect(output.string)
-          .to eq("StandardError: oops\nfrom /bin/pry:23:in `<main>'\n")
-      end
+        if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('3.2')
+          expect(output.string)
+            .to include("StandardError: oops\nfrom /bin/pry:23:in `<main>'\n")
+        else
+          expect(output.string)
+            .to include("StandardError: oops (StandardError)\nfrom /bin/pry:23:in `<main>'\n")
+        end
+      end      
     end
 
     context "when exception is a nested standard error" do
@@ -54,12 +59,21 @@ RSpec.describe Pry::ExceptionHandler do
 
       it "prints standard error message" do
         described_class.handle_exception(output, exception, pry_instance)
-        expect(output.string).to match(
-          /RuntimeError:\souter\soops\n
-           from\s.+\n
-           Caused\sby\sRuntimeError:\snested\soops\n
-           from.+/x
-        )
+        if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('3.2')
+          expect(output.string).to match(
+            /RuntimeError:\souter\soops\n
+            from\s.+\n
+            Caused\sby\sRuntimeError:\snested\soops\n
+            from.+/x
+          )
+        else
+          expect(output.string).to match(
+            /RuntimeError:\souter\soops\s\(RuntimeError\)\n
+            from\s.+\n
+            Caused\sby\sRuntimeError:\snested\soops\n
+            from\s.+/x
+          )
+        end
       end
     end
   end
